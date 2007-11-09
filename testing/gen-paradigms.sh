34e4282369657afa1c4950c72ad8bb31a14e12c9
Sjur N Moshagen <sjurnm@mac.com>
2007-11-09 09:24:12 +0000

    Updated comments.

--- testing/gen-paradigms.sh
+++ testing/gen-paradigms.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# Litt pseudo aktig, filnavn og target må være utskiftbar
+# Shell script to run paradigm generation for a list of words
+# It first cleans a bit, and then calls a perl script that does
+# the processing of the word list and calls make.
+
 rm -f paradigm-sme.txt
 rm -f *.paradigm
 perl generate-paradigm.pl parawlist.txt
