39d534eb57000b95f50c00306a6cadb9631ff568
Sjur N Moshagen <sjurnm@mac.com>
2008-04-30 13:44:38 +0000

    Convert the generated paradigms to a simple, unique list of wordforms wrapped in xml.

--- testing/gen-paradigms.pl
+++ testing/gen-paradigms.pl
@@ -11,4 +11,9 @@ while ( <> ) {
     my $command = "make $pos-paradigm WORD\=$word" ;
 #    print "$command\n";
     system($command);
+    my $xmlcommand = "egrep -v '[0-9]|Opening|Closing|bye|\\+|^$$' " .
+    	"$word.paradigm | tr -d '#' | sort -u | " .
+    	"perl -pe 's/^(.+)\$/<wordform>\\1<\\/wordform>/;' " . 
+    	" | cat pstart.xml - pend.xml > $word.xml";
+    system($xmlcommand);
 }
