01deadd7ff5a66a151bf46e5d1e657ae36f90552
Trond Trosterud <trond.trosterud@uit.no>
2008-04-30 13:12:43 +0000

    Discussion_outcome
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@18148 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: d391e9f50b1ca132a8296ad11edf575b17c06fee

--- testing/gen-paradigms.sh
+++ testing/gen-paradigms.sh
@@ -3,7 +3,13 @@
 # It first cleans a bit, and then calls a perl script that does
 # the processing of the word list and calls make.
 
+if [ $1 ]; then
+    WORDLIST=$1
+else
+    WORDLIST=parawlist.txt
+fi
+
 rm -f paradigm-sme.txt
 rm -f *.paradigm
-perl gen-paradigms.pl parawlist.txt
+perl gen-paradigms.pl $WORDLIST
 cat *.paradigm > paradigm-sme.txt
