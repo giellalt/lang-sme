ffb68fa94d2eab22006d89ea272af52e8ef82f30
Sjur Moshagen <sjurnm@mac.com>
2008-05-02 12:56:06 +0000

    Added POS as part of filename for xml file, to avoid identical entry words but different poses to overwrite each others paradigm.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@18203 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 07f7474df18479843211aaaacff3929fc33fadc0

--- testing/gen-paradigms.pl
+++ testing/gen-paradigms.pl
@@ -6,6 +6,7 @@ while ( <> ) {
     chop;
     my $word, $pos;
     ($word, $pos) = split;
+    $wordpos = $word . "_" . $pos;
 #    print "word is $word\n" ;
 #    print "pos is $pos\n" ;
     my $command = "make $pos-paradigm WORD\=$word" ;
@@ -14,6 +15,6 @@ while ( <> ) {
     my $xmlcommand = "egrep -v '[0-9]|Opening|Closing|bye|\\+|^$$' " .
     	"$word.paradigm | tr -d '#' | sort -u | " .
     	"perl -pe 's/^(.+)\$/<wordform>\\1<\\/wordform>/;' " . 
-    	" | cat pstart.xml - pend.xml > $word.xml";
+    	" | cat pstart.xml - pend.xml > $wordpos.xml";
     system($xmlcommand);
 }
