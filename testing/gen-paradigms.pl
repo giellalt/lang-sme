3fb2391c625f8a2a4162dbdeadbf0e3e27b10ef1
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2009-01-25 23:17:12 +0000

    modified scipts for generating paradigms

--- testing/gen-paradigms.pl
+++ testing/gen-paradigms.pl
@@ -2,19 +2,45 @@
 # Perl script that parses a list of word+POS lines, and for each
 # such line, calls make to generate the corresponding paradigm.
 
-while ( <> ) {
-    chop;
+use File::Spec;
+
+my $word_pos_list = @ARGV[0];
+my $xsl_script = @ARGV[1];
+my $gen_tmp = @ARGV[2];
+my $jc = "java net.sf.saxon.Transform -it main ";
+my $para_tmp = "PARA_tmp";
+my $xml_tmp = "XML_tmp";
+my $mk_dir_cmd = "mkdir $gen_tmp/$para_tmp $gen_tmp/$xml_tmp";
+
+    system($mk_dir_cmd);
+
+
+open FILE, $word_pos_list or die $!;
+
+while ( <FILE> ) {
+  chop;
+  if (/^\s.*$/) {
+    next;
+  } else {
     my $word, $pos;
     ($word, $pos) = split;
-    $wordpos = $word . "_" . $pos;
-#    print "word is $word\n" ;
-#    print "pos is $pos\n" ;
-    my $command = "make $pos-paradigm WORD\=$word make-dictindex" ;
-#    print "$command\n";
+    $word_pos = $word . "_" . $pos;
+    #    print "word is $word\n" ;
+    #    print "pos is $pos\n" ;^
+    my $command = "make $pos-paradigm WORD\=$word GEN_TMP\=$gen_tmp PARA_TMP\=$para_tmp" ;
+    #    print "$command\n";
     system($command);
-    my $xmlcommand = "egrep -v '[0-9]|Opening|Closing|bye|\\+|^$$' " .
-    	"$word.paradigm | tr -d '#' | sort -u | " .
-    	"perl -pe 's/^(.+)\$/<wordform>\\1<\\/wordform>/;' " . 
-    	" | cat pstart.xml - pend.xml > $wordpos.xml";
+    my $xmlcommand = "perl paradigm2xml.pl $gen_tmp/$para_tmp/$word.paradigm > $gen_tmp/$xml_tmp/$word_pos.xml";
     system($xmlcommand);
+    
+    $abs_path = File::Spec->rel2abs("$gen_tmp/$xml_tmp/$word_pos.xml");
+    
+    my $xslcommand = "$jc $xsl_script file=$abs_path path=$gen_tmp";
+    
+    #     print "$xslcommand\n";
+    
+    system($xslcommand);
+    
+  }
+  
 }
