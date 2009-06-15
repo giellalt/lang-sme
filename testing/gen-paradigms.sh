c4335b17e562b933729cef1a694624ba42ea5fcd
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2009-06-15 13:13:46 +0000

    Updated files for generating paradigs for dicts.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@26484 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 3891ad46c5b8bc466f8e4035b4df478ee4b13828

--- testing/gen-paradigms.sh
+++ testing/gen-paradigms.sh
@@ -3,6 +3,10 @@
 # It first cleans a bit, and then calls a perl script that does
 # the processing of the word list and calls make.
 
+
+XSLTSHEET=~cipriangerstenberger/gtsvn/words/dicts/scripts/restruct_paradigme.xsl
+GEN_TMP=Gen_tmp
+
 if [ $1 ]; then
     WORDLIST=$1
     XSLTSHEET=$2
--- testing/paradigm2xml.pl
+++ testing/paradigm2xml.pl
@@ -35,12 +35,44 @@ while (<>) {
     $morph =~ s/\+/\_/g;
     if (!$hasLemma) {
       $xmlOutput = $xmlOutput."  <lemma>$lemma</lemma>\n";
+
+      if ($pos eq 'n') {
+	# print STDERR "::: and the pos is --- $pos and the morph is $morph\n";
+	if ($morph =~ /^Prop_(.+)/) {
+	  # print STDERR "---:::--- and the morph is $morph\n";
+	  $pos = "prop";
+	  $morph = $1;
+	} elsif ($morph =~ /^Actor_(.+)/) {
+	  # print STDERR "---:::--- and the morph is $morph\n";
+	  $pos = "actor";
+	  $morph = $1;
+	  # print STDERR "---:::--- and the morph DANACH is $morph\n";
+	}
+      } elsif ($pos eq 'g3') {
+	print STDERR "::: and the pos is --- $pos and the morph is $morph\n";
+	if ($morph =~ /^N_(.+)/) {
+	  # print STDERR "---:::--- and the morph is $morph\n";
+	  $morph = $1;
+	}
+      } elsif ($pos eq 'pron') {
+	print STDERR "::: and the pos is --- $pos and the morph is $morph\n";
+	if ($morph =~ /^Indef_(.+)/) {
+	  # print STDERR "---:::--- and the morph is $morph\n";
+	  $morph = $1;
+	}
+      }
+      
       $xmlOutput = $xmlOutput."  <pos>$pos</pos>\n";
       $xmlOutput = $xmlOutput."  <analysis ms=\"$morph\">\n";
+      
       $hasLemma = 1;
       $dashedLemma = ($lemma  =~ m/[^\-]+[\-].+/);
     } else {
       $xmlOutput = $xmlOutput."  </analysis>\n";
+      if (($morph =~ /^Actor_(.+)/) || ($morph =~ /^Prop_(.+)/) || ($morph =~ /^N_(.+)/) || ($morph =~ /^Indef_(.+)/)) {
+	$morph = $1;
+      }
+      
       $xmlOutput = $xmlOutput."  <analysis ms=\"$morph\">\n";
     }
     
