2e2306ebc0af717f7050e3a277661f0228dceb5e
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2009-06-16 07:52:50 +0000

    Tiny change of the test file.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@26493 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 8731dec6ecda4e71f510c2a1f7e4cbf7a68f10d7

--- testing/run_generation.sh
+++ testing/run_generation.sh
@@ -1,2 +1,2 @@
 
-./gen-paradigms.sh cips_dictwordssss.txt ~cipriangerstenberger/gtsvn/words/dicts/scripts/restruct_paradigme.xsl Gen_tmp
+./gen-paradigms.sh cips_dictwordssss.txt ~/gtsvn/words/dicts/scripts/restruct_paradigme.xsl Gen_tmp
