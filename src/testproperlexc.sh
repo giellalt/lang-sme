19a651909a7c86a8f96b5e1daa6160b689a85068
Lene Antonsen <lene.antonsen@uit.no>
2011-10-27 10:20:09 +0000

    Forklaring.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@48952 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 87bc2f480b366261fe4a6a94979fee909f9688eb

--- src/testproperlexc.sh
+++ src/testproperlexc.sh
@@ -1,11 +1,14 @@
+# Dette skriptet tester at alle lemmaene i propernoun-sme-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingProperLemmas
+
 grep ";" propernoun-sme-lex.txt | grep -v "^\!" | egrep -v '(Attr|FirstTag|-atr)' | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "#" | tr "€" " " | tr "¢" ":" | sort -u > props
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analprops 
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "^$" >> analprops 
 sort -u -o analprops analprops 
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" > missingProperLemmas
 comm -23 props analprops >> missingProperLemmas
-grep ";" propernoun-sme-lex.txt | grep -v "^\!" | egrep '(Attr|-atr)' | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "#" | tr "€" " " |  sort -u > attrprops
+grep ";" propernoun-sme-lex.txt | grep -v "^\!" | egrep '(Attr|-atr|FirstTag)' | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "#" | tr "€" " " |  sort -u > attrprops
 cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" >> missingProperLemmas
 cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 |  grep -v "N+" | grep -v "^$" | sort -u > analattrprops  
 comm -23 attrprops analattrprops >> missingProperLemmas
 rm *props
+open missingProperLemmas
