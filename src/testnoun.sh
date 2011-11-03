2131f2d575dac2a0b38553d8fab122ae202e35b5
Lene Antonsen <lene.antonsen@uit.no>
2011-11-03 12:39:32 +0000

    Forbedring, fjerner % fra lemmaet.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@49346 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: b7ffd7aa34c58d665fce869d45b5950165b43871

--- src/testnoun.sh
+++ src/testnoun.sh
@@ -1,9 +1,10 @@
 # Dette skriptet tester at nesten alle lemmaene i noun-sme-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingnounLemmas.txt
 
-grep ";" noun-sme-lex.txt | grep -v "^\!" | egrep -v '( R | Rreal | Rnoun | RHyph |CmpN/Only|+Gen+|+Loc+|CmpN/Last)' | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "#" | tr "€" " " | tr "¢" ":" | sort -u > nouns
+grep ";" noun-sme-lex.txt | grep -v "^\!" | egrep -v '( R | Rreal | Rnoun | RHyph |CmpN/Only|+Gen+|+Loc+|CmpN/Last)' | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "#%" | tr "€" " " | tr "¢" ":" | sort -u > nouns
 cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analnouns 
 cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "^$" >> analnouns 
 sort -u -o analnouns analnouns 
 comm -23 nouns analnouns > missingnounLemmas.txt
 rm *nouns
 open missingnounLemmas.txt
+
