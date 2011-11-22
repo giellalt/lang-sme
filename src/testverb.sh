7432ab061206560d8d74fbfb6da419db9b993dd6
Lene Antonsen <lene.antonsen@uit.no>
2011-11-22 10:43:03 +0000

    Rettinger
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@50152 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: f31baa4e596f27c57d0bca79dc69849c094eefde

--- src/testverb.sh
+++ src/testverb.sh
@@ -1,9 +1,7 @@
 # Dette skriptet tester at nesten alle lemmaene i verb-sme-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingverbLemmas.txt
 
-grep ";" verb-sme-lex.txt | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|+V)' | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > verbs
+grep ";" verb-sme-lex.txt | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|\+V)' | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > verbs
 cat verbs | sed 's/$/+V+Inf/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "V+" | grep -v "^$" | sort -u > analverbs 
-cat verbs | sed 's/$/+V+Inf/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "^$" >> analverbs 
-sort -u -o analverbs analverbs 
 comm -23 verbs analverbs > missingverbLemmas.txt
 rm *verbs
 open missingverbLemmas.txt
