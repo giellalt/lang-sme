5a6c22d20d4321e6b70fc3bc2274d7f74abaeac1
Sjur Moshagen <sjurnm@mac.com>
2012-02-03 16:53:35 +0000

    Second part of major rewrite of the morphology and phonology test bench for SME. * twol testing is the default - just type 'make' * twol testing automatically runs both positive and negative tests * the paradigm testing is rewritten to use one of three different generators: standard, dictionary or oahpa. This is controlled by specifying PARATYPE as an option to make * some smaller tweaks * the perl script is enhanced to understand the PARATYPE argument, as well as the shell script * the shell script will now give some help if given -h
    There are certainly bugs and missing features. Please test and improve or file a bug report.
    
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@53580 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 38c5f36c4f915622cc0d01b9fc57ee2cb44cede6

--- testing/gen-paradigms.sh
+++ testing/gen-paradigms.sh
@@ -4,17 +4,29 @@
 # the processing of the word list and calls make.
 
 
-XSLTSHEET=~cipriangerstenberger/gtsvn/words/dicts/scripts/restruct_paradigme.xsl
+XSLTSHEET=$GTHOME/words/dicts/scripts/restruct_paradigme.xsl
 GEN_TMP=Gen_tmp
+
+if [ $1 == '-h' ]; then
+    echo "Usage:"
+    echo "$0"
+    echo "	Will generate paradigms for all words in parawlist.txt"
+    echo "$0 WORDLIST XSLTSHEET OUTPUTDIR PARATYPE"
+    echo "	Will generate paradigms for all words in WORDLIST,"
+    echo "	wrapping them in xml using XSLTSHEET, place the result in"
+    echo "	OUTPUTDIR, and the paradigms will be of type PARATYPE."
+    echo "	Recognised values for PARATYPE are: default, dict, oahpa."
+    exit
+fi
 
 if [ $1 ]; then
     WORDLIST=$1
     XSLTSHEET=$2
     GEN_TMP=$3
+    PARATYPE=$4
 else
     WORDLIST=parawlist.txt
 fi
 
-rm -rf  $GEN_TMP/*
-perl gen-paradigms.pl $WORDLIST $XSLTSHEET $GEN_TMP
-
+rm -rf  $GEN_TMP
+perl gen-paradigms.pl $WORDLIST $XSLTSHEET $GEN_TMP $PARATYPE
