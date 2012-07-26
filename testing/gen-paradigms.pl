2edc225b08e9ade46ff2176c6ab014841ae63331
Ciprian Gerstenberger <ciprian.gerstenberger@gmail.com>
2012-07-26 10:15:00 +0000

    use the make-gen-dict file
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@60443 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: bed15c8f44f39ca5db629c119b30dd341955e38a

--- testing/gen-paradigms.pl
+++ testing/gen-paradigms.pl
@@ -29,7 +29,7 @@ while ( <FILE> ) {
       $word_pos = $word . "_" . $pos;
       print "word is $word\n" ;
       print "pos is $pos\n" ;
-      my $command = "make paradigm POS\=$pos WORD\=$word GEN_TMP\=$gen_tmp PARATYPE\=$paratype" ;
+      my $command = "make -f make-gen-dict paradigm POS\=$pos WORD\=$word GEN_TMP\=$gen_tmp PARATYPE\=$paratype" ;
       #    print "$command\n";
       system($command);
       my $xmlcommand = "perl paradigm2xml.pl $gen_tmp/$para_tmp/$word_pos.paradigm > $gen_tmp/$xml_tmp/$word_pos.xml";
