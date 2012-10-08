cea06a60ed0c09efea7d5160aeed617775be06b4
Lene Antonsen <lene.antonsen@uit.no>
2012-10-08 08:11:48 +0000

    GTBIG istedenfor biggies
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@63721 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 4b01d3930819b3aabff8058a6c420f412b1a13c7

--- script/testCGrules.sh
+++ script/testCGrules.sh
@@ -1,7 +1,7 @@
 # Skript for å teste analysen av testkorpus.txt med sme.fst og sme-dis.rle opp mot gullversjon.
 
 # Analyserer testkorpuset:
-cat ~/biggies/gt/sme/corp/testkorpus.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abbr.txt | $LOOKUP $GTHOME/gt/sme/bin/sme.fst | lookup2cg | vislcg3 -g $GTHOME/gt/sme/src/sme-dis.rle > $GTHOME/gt/sme/dev/testdis
+cat $GTBIG/gt/sme/corp/testkorpus.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abbr.txt | $LOOKUP $GTHOME/gt/sme/bin/sme.fst | lookup2cg | vislcg3 -g $GTHOME/gt/sme/src/sme-dis.rle > $GTHOME/gt/sme/dev/testdis
 
 # Fjerner semantiske tagger og #:
 cat $GTHOME/gt/sme/dev/testdis | perl -pe 's/(Time|Ani|Hum|Org|Obj|Build|Clth|Wthr|Food|Body|Event|Plc|Veh|Txt|Route|Plant|Allegro|Group|v1|v2|v3|v4) //g' | tr -d "#" > $GTHOME/gt/sme/dev/cleantest
@@ -10,7 +10,7 @@ cat $GTHOME/gt/sme/dev/testdis | perl -pe 's/(Time|Ani|Hum|Org|Obj|Build|Clth|Wt
 perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/cleantest | uniq > $GTHOME/gt/sme/dev/sortedtestdis
 
 # Henter gullstandard, fjerner Allegro-tag som foreløpig ikke fungerer som den skal:
-cat ~/biggies/gt/sme/corp/correct/testkorpus.dis.corr.txt | sed 's/Allegro //' > $GTHOME/gt/sme/dev/cleantestkorpus.dis.corr.txt
+cat $GTBIG/gt/sme/corp/correct/testkorpus.dis.corr.txt | sed 's/Allegro //' > $GTHOME/gt/sme/dev/cleantestkorpus.dis.corr.txt
 
 # Sorterer alfabetisk inne i cohortene
 perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/cleantestkorpus.dis.corr.txt | uniq > $GTHOME/gt/sme/dev/sortedtestkorpus 
