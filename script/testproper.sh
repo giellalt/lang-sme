7dc7760c7c9963fc7568ef0d57d6423744e63599
leneantonsen <lene.antonsen@uit.no>
2012-10-16 07:28:32 +0000

    vi fjerner ikke # i lemma

--- script/testproper.sh
+++ script/testproper.sh
@@ -1,12 +1,12 @@
 # Dette skriptet tester at alle lemmaene i propernoun-sme-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingProperLemmas.txt
 
-grep ";" $GTHOME/gt/sme/src/propernoun-sme-lex.txt | grep -v "^\!" | egrep -v '(Attr|FirstTag|-atr)' | sed 's/% /€/g' | sed 's/%:/¢/g' | sed 's/%!/!/g' | tr ":+" " " | cut -d " " -f1 | tr -d "#" | tr "€" " " | tr "¢" ":" | sort -u > props
+grep ";" $GTHOME/gt/sme/src/propernoun-sme-lex.txt | grep -v "^\!" | egrep -v '(Attr|FirstTag|-atr)' | sed 's/% /€/g' | sed 's/%:/¢/g' | sed 's/%!/!/g' | tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > props
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analprops 
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "^$" >> analprops 
 sort -u -o analprops analprops 
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" > missingProperLemmas.txt
 comm -23 props analprops >> missingProperLemmas.txt
-grep ";" $GTHOME/gt/sme/src/propernoun-sme-lex.txt | grep -v "^\!" | egrep '(Attr|-atr|FirstTag)' | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "#" | tr "€" " " |  sort -u > attrprops
+grep ";" $GTHOME/gt/sme/src/propernoun-sme-lex.txt | grep -v "^\!" | egrep '(Attr|-atr|FirstTag)' | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " |  sort -u > attrprops
 cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" >> missingProperLemmas.txt
 cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 |  grep -v "N+" | grep -v "^$" | sort -u > analattrprops  
 comm -23 attrprops analattrprops >> missingProperLemmas.txt
