1261a46db519584e720327ec7c26ae1bb9a2ff95
leneantonsen <lene.antonsen@uit.no>
2012-10-16 06:25:42 +0000

    Retting

--- src/testproper.sh
+++ src/testproper.sh
@@ -7,7 +7,7 @@ sort -u -o analprops analprops
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" > missingProperLemmas.txt
 comm -23 props analprops >> missingProperLemmas.txt
 grep ";" propernoun-sme-lex.txt | grep -v "^\!" | egrep '(Attr|-atr|FirstTag)' | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "#" | tr "€" " " |  sort -u > attrprops
-cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" >> missingProperLemmas
+cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" >> missingProperLemmas.txt
 cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 |  grep -v "N+" | grep -v "^$" | sort -u > analattrprops  
 comm -23 attrprops analattrprops >> missingProperLemmas.txt
 rm *props
