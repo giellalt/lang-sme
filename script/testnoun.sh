d113a0ac9f85bc48a97a857eef1799d3e052c42c
leneantonsen <lene.antonsen@uit.no>
2012-10-16 06:31:56 +0000

    Flyttet testskriptene til en høveligere mappe.

---
+++ script/testnoun.sh
@@ -0,0 +1,14 @@
+# Dette skriptet tester at nesten alle lemmaene i noun-sme-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingnounLemmas.txt
+
+grep ";" src/noun-sme-lex.txt | grep -v "^\!" | egrep -v '(CmpN/Only|CmpN/Last|ShCmp|RCmpnd)' | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "#%" | tr "€" " " | tr "¢" ":" | sort -u > nouns
+cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analnouns 
+cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "^$" >> analnouns 
+grep ";" src/noun-sme-lex.txt | grep -v "^\!" | grep -v ShCmp | egrep "( R | Rreal | Rnoun)" | cut -d ":" -f1 | sed 's/+Use\/Sub//' | sed 's/CmpN\/Pref+CmpN\/SgN$/Cmp#viessu+N+Sg+Nom/' |  sed 's/CmpN\/Pref+CmpN\/SgG$/Cmp#viessu+N+Sg+Nom/' | sed 's/CmpN\/PlG$/Cmp#viessu+N+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "\-" | grep -v "\-.*\-" | cut -d "-" -f1 >> analnouns
+grep ";" src/noun-sme-lex.txt | grep -v "^\!" | grep -v ShCmp | egrep "( R | Rreal | Rnoun)" | cut -d ":" -f1 | sed 's/+Use\/Sub//' | sed 's/CmpN\/Pref+CmpN\/SgN$/Cmp#viessu+N+Sg+Nom/' |  sed 's/CmpN\/Pref+CmpN\/SgG$/Cmp#viessu+N+Sg+Nom/' | sed 's/CmpN\/PlG$/Cmp#viessu+N+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "\-" | grep "\-.*\-" | cut -d "-" -f1,2 >> analnouns
+grep ";" src/noun-sme-lex.txt | grep -v "^\!" | grep -v ShCmp | grep "RHyph" | cut -d ":" -f1 | sed 's/+Use\/Sub//' | sed 's/CmpN\/Pref+CmpN\/SgN$/Cmp-#viessu+N+Sg+Nom/' |  sed 's/CmpN\/Pref+CmpN\/SgG$/Cmp-#viessu+N+Sg+Nom/' |  sed 's/CmpN\/PlG$/Cmp-#viessu+N+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "\-" | cut -d "-" -f1 >> analnouns
+sort -u -o nouns nouns 
+sort -u -o analnouns analnouns 
+comm -23 nouns analnouns > missingnounLemmas.txt
+rm *nouns
+open missingnounLemmas.txt
+
