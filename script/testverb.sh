7803674166fd353938fe4fb8c3f125c1b96c1316
leneantonsen <lene.antonsen@uit.no>
2012-10-16 06:37:05 +0000

    La til globale stier

--- script/testverb.sh
+++ script/testverb.sh
@@ -1,6 +1,6 @@
 # Dette skriptet tester at nesten alle lemmaene i verb-sme-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingverbLemmas.txt
 
-grep ";" src/verb-sme-lex.txt | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|\+V)' | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > verbs
+grep ";" $GTHOME/gt/sme/src/verb-sme-lex.txt | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|\+V)' | tr ":+" " " | cut -d " " -f1 | tr -d "#" | sort -u > verbs
 cat verbs | sed 's/$/+V+Inf/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "V+" | grep -v "^$" | sort -u > analverbs 
 comm -23 verbs analverbs > missingverbLemmas.txt
 rm *verbs
