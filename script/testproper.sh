2c533dabcec0ebd2838550fc57a0bca6715cb03d
leneantonsen <lene.antonsen@uit.no>
2012-10-16 15:01:02 +0000

    Missinglistene lagres i dev/ og alle mellomfiler strykes. Dette gjør det ryddigere.

--- script/testproper.sh
+++ script/testproper.sh
@@ -4,11 +4,11 @@ grep ";" $GTHOME/gt/sme/src/propernoun-sme-lex.txt | grep -v "^\!" | egrep -v '(
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analprops 
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "^$" >> analprops 
 sort -u -o analprops analprops 
-cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" > missingProperLemmas.txt
-comm -23 props analprops >> missingProperLemmas.txt
+cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" > $GTHOME/gt/sme/dev/missingProperLemmas.txt
+comm -23 props analprops >> $GTHOME/gt/sme/dev/missingProperLemmas.txt
 grep ";" $GTHOME/gt/sme/src/propernoun-sme-lex.txt | grep -v "^\!" | egrep '(Attr|-atr|FirstTag)' | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " |  sort -u > attrprops
-cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" >> missingProperLemmas.txt
+cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep "N+" >> $GTHOME/gt/sme/dev/missingProperLemmas.txt
 cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 |  grep -v "N+" | grep -v "^$" | sort -u > analattrprops  
-comm -23 attrprops analattrprops >> missingProperLemmas.txt
+comm -23 attrprops analattrprops >> $GTHOME/gt/sme/dev/missingProperLemmas.txt
 rm *props
-open missingProperLemmas.txt
+open $GTHOME/gt/sme/dev/missingProperLemmas.txt
