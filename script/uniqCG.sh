06c6340b5a4126012b7b86acc31c47ec7666f732
Ritva Nystad <ritva.nystad@uit.no>
2013-01-11 15:21:56 +0000

    sme-dis.rle

--- script/uniqCG.sh
+++ script/uniqCG.sh
@@ -1,10 +1,10 @@
 # Skript for å kjøre sum-cg.pl uten semtagger:
 
 # Fjerner sem-tagger:
-cat dev/muitalus_dis.txt | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4) //g' > dev/test_clean
+cat dev/1000_11jan | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4) //g' > dev/test_clean
 
 # Sorterer og unifiserer analysene i hver cohort:
 perl ../script/sort-cg-cohort.pl dev/test_clean | uniq > dev/test_uniq
 
 # Kjør sum-cg.pl
-perl ../script/sum-cg.pl --grammar dev/test_uniq|less
+perl ../script/sum-cg.pl --grammar dev/test_uniq|see 
