bd6984bcea828c6fd0838b0f111184c19b008661
Ritva Nystad <ritva.nystad@uit.no>
2013-01-24 14:50:20 +0000

    Added constraint and example sentence. Changed order of two example sentences.

--- script/uniqCG.sh
+++ script/uniqCG.sh
@@ -1,7 +1,7 @@
 # Skript for å kjøre sum-cg.pl uten semtagger:
 
 # Fjerner sem-tagger:
-cat dev/1000_11jan | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4) //g' > dev/test_clean
+cat dev/1000_4jan | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4) //g' > dev/test_clean
 
 # Sorterer og unifiserer analysene i hver cohort:
 perl ../script/sort-cg-cohort.pl dev/test_clean | uniq > dev/test_uniq
