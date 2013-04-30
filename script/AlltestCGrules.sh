eafc184b48a4874d53d346d0b42a4abc4dfd8f6e
Lene Antonsen <lene.antonsen@uit.no>
2013-04-30 09:57:21 +0000

    Tok høyde for <vdic>
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@75553 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 1928df42d06358260eb2596d5cf985b7c1bc721d

--- script/AlltestCGrules.sh
+++ script/AlltestCGrules.sh
@@ -6,7 +6,7 @@ cat $GTBIG/gt/sme/corp/testkorpus.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abb
 cat $GTBIG/gt/sme/corp/divgullkorpus.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abbr.txt | $LOOKUP $GTHOME/gt/sme/bin/sme.fst | lookup2cg | vislcg3 -g $GTHOME/gt/sme/src/Nsme-dis.rle >> $GTHOME/gt/sme/dev/testdis
 
 # Fjerner semantiske tagger og #:
-cat $GTHOME/gt/sme/dev/testdis | perl -pe 's/(Lang|Money|Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4) //g' | tr -d "#"  | cut -d "@" -f1 > $GTHOME/gt/sme/dev/cleantest
+cat $GTHOME/gt/sme/dev/testdis | perl -pe 's/(Lang|Money|Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>) //g' | tr -d "#"  | cut -d "@" -f1 > $GTHOME/gt/sme/dev/cleantest
 
 # Sorterer alfabetisk inne i cohortene
 perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/cleantest | uniq > $GTHOME/gt/sme/dev/sortedtestdis
@@ -32,6 +32,6 @@ echo " " >> dev/AlltestCG.txt
 echo "Den nye analysen inneholder analyser som ikke finnes i gullstandarden:" >> dev/AlltestCG.txt
 cat dev/testCG_result.txt | grep '^>' | perl -pe 's/(TV|IV|G3|V\*|V\*\*) //' | cut -d '"' -f3 | rev | awk -F' ' '{print $1" "$2}' | rev | sort | uniq -c | sort -nr >> dev/AlltestCG.txt
 see dev/AlltestCG.txt
-cat dev/testCG_result.txt | sed 's/$/¢/' |sed 's/---¢/€/' | tr "\n" " " | sed 's/¢ €/€/g' |tr "¢" "\n" | grep € | grep ' V ' | rev | sort | rev > dev/AlltestCGverbs.txt
+cat dev/testCG_result.txt | sed 's/$/¢/' |sed 's/---¢/€/' | tr "\n" " " | sed 's/¢ €/€/g' |tr "¢" "\n" | grep € | grep ' V '  > dev/AlltestCGverbs.txt
 
 
