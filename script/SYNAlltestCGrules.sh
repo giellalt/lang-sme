f095807d075b8731b9b58e4f4bc5606b8f1d0cbb
Lene Antonsen <lene.antonsen@uit.no>
2013-05-27 13:24:01 +0000

    Nytt skript som også tar med syn-tagger. Skriptet viser at her er det den del arbeid å gjøre.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@76797 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 7902b29a7666e716af1a0435aece1bfdd81a3883

---
+++ script/SYNAlltestCGrules.sh
@@ -0,0 +1,39 @@
+# Skript for å teste analysen av testkorpus.txt med sme.fst og sme-dis.rle opp mot gullversjon.
+
+# Analyserer testkorpusene:
+cat $GTBIG/gt/sme/corp/testkorpus.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abbr.txt | $LOOKUP $GTHOME/gt/sme/bin/sme.fst | lookup2cg | vislcg3 -g $GTHOME/gt/sme/src/Nsme-dis.rle > $GTHOME/gt/sme/dev/SYNtestdis
+
+cat $GTBIG/gt/sme/corp/divgullkorpus.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abbr.txt | $LOOKUP $GTHOME/gt/sme/bin/sme.fst | lookup2cg | vislcg3 -g $GTHOME/gt/sme/src/Nsme-dis.rle >> $GTHOME/gt/sme/dev/SYNtestdis
+
+# Fjerner semantiske tagger og #:
+
+cat $GTHOME/gt/sme/dev/SYNtestdis | perl -pe 's/(Lang|Money|Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g' | tr -d "#" > $GTHOME/gt/sme/dev/SYNcleantest
+
+
+# Sorterer alfabetisk inne i cohortene
+perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/SYNcleantest | uniq > $GTHOME/gt/sme/dev/SYNsortedtestdis
+
+# Henter gullstandarder, fjerner Allegro-tag som foreløpig ikke fungerer som den skal:
+cat $GTBIG/gt/sme/corp/correct/testkorpus.N.corr.txt | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g' > $GTHOME/gt/sme/dev/SYNcleantestkorpus.dis.corr.txt
+perl $GTHOME/gt/script/sort-cg-cohort.pl $GTBIG/gt/sme/corp/correct/divgullkorpus.N.corr.txt | uniq >> $GTHOME/gt/sme/dev/SYNcleantestkorpus.dis.corr.txt
+
+# Sorterer alfabetisk inne i cohortene
+perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/SYNcleantestkorpus.dis.corr.txt | uniq > $GTHOME/gt/sme/dev/SYNsortedtestkorpus 
+
+diff -w $GTHOME/gt/sme/dev/SYNsortedtestkorpus $GTHOME/gt/sme/dev/SYNsortedtestdis > $GTHOME/gt/sme/dev/SYNtestCG_result.txt
+
+# Antall ulike linjer:
+echo "Antall disambiguert annerledes enn gullstandard:" > dev/SYNAlltestCG.txt
+cat dev/SYNtestCG_result.txt | grep '^<' | wc -l >> dev/SYNAlltestCG.txt
+echo "Antall ikke disambiguert eller uriktig disambiguert:" >> dev/SYNAlltestCG.txt
+cat dev/SYNtestCG_result.txt | grep '^>' | wc -l >> dev/SYNAlltestCG.txt
+echo " " >> dev/SYNAlltestCG.txt
+echo "Gullstandarden inneholder analyser som ikke finnes i den nye analysen:" >> dev/SYNAlltestCG.txt
+cat dev/SYNtestCG_result.txt | grep '^<' | perl -pe 's/(TV|IV|G3|V\*|V\*\*) //' | cut -d '"' -f3 | rev | awk -F' ' '{print $1" "$2}' | rev | sort | uniq -c | sort -nr >> dev/SYNAlltestCG.txt
+echo " " >> dev/SYNAlltestCG.txt
+echo "Den nye analysen inneholder analyser som ikke finnes i gullstandarden:" >> dev/SYNAlltestCG.txt
+cat dev/SYNtestCG_result.txt | grep '^>' | perl -pe 's/(TV|IV|G3|V\*|V\*\*) //' | cut -d '"' -f3 | rev | awk -F' ' '{print $1" "$2}' | rev | sort | uniq -c | sort -nr >> dev/SYNAlltestCG.txt
+see dev/SYNAlltestCG.txt
+#cat dev/SYNtestCG_result.txt | sed 's/$/¢/' |sed 's/---¢/€/' | tr "\n" " " | sed 's/¢ €/€/g' |tr "¢" "\n" | grep € | rev | sort | rev | see
+
+
