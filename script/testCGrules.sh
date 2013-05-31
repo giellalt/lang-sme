c710446972e710ff4d32dcff27b6bcc8004f5681
Lene Antonsen <lene.antonsen@uit.no>
2013-05-31 10:24:46 +0000

    La til cut for @
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@77021 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 954a046a9228c76fd8c8e9cdac17e6dd2c2e549b

--- script/testCGrules.sh
+++ script/testCGrules.sh
@@ -1,17 +1,16 @@
-# Skript for å teste analysen av testkorpus.txt og divgullkorpus.txt med sme.fst og sme-dis.rle opp mot gullversjon.
+# Skript for å teste analysen av testkorpus.txt med sme.fst og sme-dis.rle opp mot gullversjon.
 
-# Analyserer testkorpusene:
+# Analyserer testkorpuset:
 cat $GTBIG/gt/sme/corp/testkorpus.txt $GTBIG/gt/sme/corp/divgullkorpus.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abbr.txt | $LOOKUP $GTHOME/gt/sme/bin/sme.fst | lookup2cg | vislcg3 -g $GTHOME/gt/sme/src/sme-dis.rle > $GTHOME/gt/sme/dev/testdis
 
-# Fjerner semantiske tagger, # osv:
-
-cat $GTHOME/gt/sme/dev/testdis | perl -pe 's/(Lang|Money|Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g' | tr -d "#" > $GTHOME/gt/sme/dev/cleantestdis
+# Fjerner semantiske tagger og #:
+cat $GTHOME/gt/sme/dev/testdis | perl -pe 's/(Lang|Money|Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g' | tr -d "#" | cut -d "@" -f1 > $GTHOME/gt/sme/dev/cleantestdis
 
 # Sorterer alfabetisk inne i cohortene
 perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/cleantestdis | uniq > $GTHOME/gt/sme/dev/sortedtestdis
 
 # Henter gullstandarder, fjerner semantiske tagger, # osv :
-cat $GTBIG/gt/sme/corp/correct/testkorpus.N.corr.txt $GTBIG/gt/sme/corp/correct/divgullkorpus.N.corr.txt | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g' > $GTHOME/gt/sme/dev/cleangullkorpus.dis.corr.txt
+cat $GTBIG/gt/sme/corp/correct/testkorpus.N.corr.txt $GTBIG/gt/sme/corp/correct/divgullkorpus.N.corr.txt | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g'  | cut -d "@" -f1 > $GTHOME/gt/sme/dev/cleangullkorpus.dis.corr.txt
 
 # Sorterer alfabetisk inne i cohortene
 perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/cleangullkorpus.dis.corr.txt | uniq > $GTHOME/gt/sme/dev/sortedgullkorpus 
@@ -31,5 +30,3 @@ echo "Den nye analysen inneholder analyser som ikke finnes i gullstandarden:" >>
 cat dev/testCG_result.txt | grep '^>' | perl -pe 's/(TV|IV|G3|V\*|V\*\*) //' | cut -d '"' -f3 | rev | awk -F' ' '{print $1" "$2}' | rev | sort | uniq -c | sort -nr >> dev/testCG.txt
 see dev/testCG.txt
 #cat dev/testCG_result.txt | sed 's/$/¢/' |sed 's/---¢/€/' | tr "\n" " " | sed 's/¢ €/€/g' |tr "¢" "\n" | grep € | grep '@' | see
-
-
