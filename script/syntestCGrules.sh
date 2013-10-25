209584dcca0b243fc41c45bdf2f345252b579fc7
Lene Antonsen <lene.antonsen@uit.no>
2013-10-25 12:13:25 +0000

    Forbedra versjon
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@82754 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: 9dace0740855f75e073d0924f55593491d7aa071

--- script/syntestCGrules.sh
+++ script/syntestCGrules.sh
@@ -5,13 +5,13 @@ cat $GTBIG/gt/sme/corp/testkorpus.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abb
 
 # Fjerner semantiske tagger, # osv:
 
-cat $GTHOME/gt/sme/dev/syntestdis | perl -pe 's/(Lang|Money|Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g' | sed 's/ Act / /' | tr -d "#" > $GTHOME/gt/sme/dev/syncleantestdis
+cat $GTHOME/gt/sme/dev/syntestdis | perl -pe 's/(Sem\/Lang|Sem\/Money|Sem\/Ani|Sem\/Body|Sem\/Build|Sem\/Clth|Sem\/Edu|Sem\/Event|Sem\/Fem|Sem\/Food|Sem\/Group|Sem\/Hum|Sem\/Mal|Sem\/Measr|Sem\/Obj|Sem\/Org|Sem\/Plant|Sem\/Plc|Sem\/Route|Sem\/Sur|Sem\/Time|Sem\/Txt|Sem\/Veh|Sem\/Wpn|Sem\/Wthr|Sem\/Mat|Sem\/Semcon|Sem\/Ctain|Sem\/Date|Sem\/Act|Allegro|v1|v2|v3|v4|<vdic>) //g' | tr -d "#" > $GTHOME/gt/sme/dev/syncleantestdis
 
 # Sorterer alfabetisk inne i cohortene
 perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/syncleantestdis | uniq > $GTHOME/gt/sme/dev/synsortedtestdis
 
 # Henter gullstandarder, fjerner semantiske tagger, # osv :
-cat $GTBIG/gt/sme/corp/correct/testkorpus.dis.corr.txt | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g' | sed 's/ Act / /'  > $GTHOME/gt/sme/dev/syncleangullkorpus.dis.corr.txt
+cat $GTBIG/gt/sme/corp/correct/testkorpus.dis.corr.txt | perl -pe 's/(Sem\/Lang|Sem\/Money|Sem\/Ani|Sem\/Body|Sem\/Build|Sem\/Clth|Sem\/Edu|Sem\/Event|Sem\/Fem|Sem\/Food|Sem\/Group|Sem\/Hum|Sem\/Mal|Sem\/Measr|Sem\/Obj|Sem\/Org|Sem\/Plant|Sem\/Plc|Sem\/Route|Sem\/Sur|Sem\/Time|Sem\/Txt|Sem\/Veh|Sem\/Wpn|Sem\/Wthr|Sem\/Mat|Sem\/Semcon|Sem\/Ctain|Sem\/Date|Sem\/Act|Allegro|v1|v2|v3|v4|<vdic>) //g'  > $GTHOME/gt/sme/dev/syncleangullkorpus.dis.corr.txt
 
 # Sorterer alfabetisk inne i cohortene
 perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/syncleangullkorpus.dis.corr.txt | uniq > $GTHOME/gt/sme/dev/synsortedgullkorpus 
@@ -19,10 +19,16 @@ perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/syncleangullkorpus.d
 diff -w $GTHOME/gt/sme/dev/synsortedgullkorpus $GTHOME/gt/sme/dev/synsortedtestdis > $GTHOME/gt/sme/dev/syntestCG_result.txt
 
 # Antall ulike linjer:
-echo "Antall disambiguert annerledes enn gullstandard:" > dev/syntestCG.txt
-cat dev/syntestCG_result.txt | grep '^<' | wc -l >> dev/syntestCG.txt
-echo "Antall ikke disambiguert eller uriktig disambiguert:" >> dev/syntestCG.txt
-cat dev/syntestCG_result.txt | grep '^>' | wc -l >> dev/syntestCG.txt
+echo "Antall syntagger i gullkorpuset:" > dev/syntestCG.txt
+cat $GTHOME/gt/sme/dev/synsortedgullkorpus | grep "@" | wc -l >> dev/syntestCG.txt
+echo " " >> dev/syntestCG.txt
+echo "Antall med samme morfologisk analyse, men annen syntag enn gullstandard:" >> dev/syntestCG.txt
+cat dev/syntestCG_result.txt | sed 's/ $/¢/' | tr -d "\n" | sed 's/¢-/ /g' | tr "¢" "\n" | grep '\-\-' | sed 's/ -->//' | tr "\t" "\n" | cut -d "@" -f1 | sed 's/ $//' | uniq | tr "\n" " " | tr "<" "\n" | grep -v '".*".*".*"' | wc -l >> dev/syntestCG.txt
+echo "Antall uten syntag:" >> dev/syntestCG.txt
+cat dev/synsortedtestdis | sed 's/"</¢/' | tr "\n" " " | tr "¢" "\n" | egrep -v '(@|CLB)' | wc -l >> dev/syntestCG.txt
+echo " " >> dev/syntestCG.txt
+echo "Disse har samme morfologisk analyse, men ikke samme syntagg" >> dev/syntestCG.txt
+cat dev/syntestCG_result.txt | sed 's/ $/¢/' | tr -d "\n" | sed 's/¢-/ /g' | tr "¢" "\n" | grep '\-\-' | sed 's/ -->//' | tr "\t" "\n" | cut -d "@" -f1 | sed 's/ $//' | uniq >> dev/syntestCG.txt
 echo " " >> dev/syntestCG.txt
 echo "Gullstandarden inneholder analyser som ikke finnes i den nye analysen:" >> dev/syntestCG.txt
 cat dev/syntestCG_result.txt | grep '^<' | perl -pe 's/(TV|IV|G3|V\*|V\*\*) //' | cut -d '"' -f3 | rev | awk -F' ' '{print $1" "$2}' | rev | sort | uniq -c | sort -nr >> dev/syntestCG.txt
