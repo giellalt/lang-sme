e8cddfcdbcac5e83cc1b0ab7450d12550eb11395
Lene Antonsen <lene.antonsen@uit.no>
2013-12-12 15:07:12 +0000

    La til nye semtagger
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/gt/sme@85109 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: e80078fdb9fdaeab05e34efa843b603d04d27519

--- script/eckhard.sh
+++ script/eckhard.sh
@@ -14,7 +14,7 @@ perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/ECKH-cleantestINTERM
 
 # Henter gullstandarder, fjerner Allegro-tag som foreløpig ikke fungerer som den skal:
 cat $GTBIG/gt/sme/corp/correct/testkorpus.N.corr.txt | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g' > $GTHOME/gt/sme/dev/ECKH-cleantestkorpus.dis.corrINTERMEDIATE
-perl $GTHOME/gt/script/sort-cg-cohort.pl $GTBIG/gt/sme/corp/correct/divgullkorpus.N.corr.txt | uniq >> $GTHOME/gt/sme/dev/ECKH-cleantestkorpus.dis.corrINTERMEDIATE
+perl $GTHOME/gt/script/sort-cg-cohort.pl $GTBIG/gt/sme/corp/correct/divgullkorpus.N.corr.txt | perl -pe 's/(Ani|Body|Build|Clth|Edu|Event|Fem|Food|Group|Hum|Mal|Measr|Obj|Org|Plant|Plc|Route|Sur|Time|Txt|Veh|Wpn|Wthr|Allegro|v1|v2|v3|v4|<vdic>|Date) //g' >> $GTHOME/gt/sme/dev/ECKH-cleantestkorpus.dis.corrINTERMEDIATE
 
 # Sorterer alfabetisk inne i cohortene
 perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/gt/sme/dev/ECKH-cleantestkorpus.dis.corrINTERMEDIATE | uniq > $GTHOME/gt/sme/dev/ECKH-sortedGOLDSTANDARD
