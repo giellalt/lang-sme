0d9cc0fb5d227f90185b40f60b73c35d9d0f63f7
leneantonsen <lene.antonsen@uit.no>
2014-03-31 19:44:00 +0000

    Forbedring av skript

--- script/testadj.sh
+++ script/testadj.sh
@@ -1,14 +1,15 @@
 # Dette skriptet tester at nesten alle lemmaene i adj-sme-lex.txt kan genereres. De som ikke kan genereres, kopieres til missingadjLemmas.txt
 
-grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | egrep -v "(LEXICON|ATTR|FINJU|Der|Attr| At |BBO |MUS |BUOREMUSS|OVDDIT |STUORIBUS|BUStem | K | Rreal | R )" | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > adjs
+#grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | egrep -v "(LEXICON|ATTR|FINJU|Der|Attr| At |BBO |MUS |BUOREMUSS|OVDDIT |STUORIBUS|BUStem | K | Rreal | R )" | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > adjs
+grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | egrep -v "(LEXICON|ATTR|FINJU|Der|Attr| At |BBO |MUS |BUOREMUSS|OVDDIT |STUORIBUS|BUStem | K | Rreal | R )" | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > adjs
 cat adjs | sed 's/$/+A+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u > analadjs 
 grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | grep -v "+Gen" | grep "_PL " | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > pluradjs
 cat pluradjs | sed 's/$/+A+Pl+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
-grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | grep -v "+Gen" | egrep "(ATTR|Attr)" | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > attradjs
+grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | grep -v "+Gen" | egrep "(ATTR|Attr)" | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > attradjs
 cat attradjs | sed 's/$/+A+Attr/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
 grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | grep "BUOREMUSS" | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > superladjs
 cat superladjs | sed 's/$/+A+Superl+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
-grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | grep "(OVDDIT|BBO |BU/MUS|STUORIBUS|BUStem)" | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > compladjs
+grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | egrep "(OVDDIT|BBO |BU/MUS|EABBO/EAMOS|STUORIBUS|BUStem)" | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > compladjs
 cat compladjs | sed 's/$/+A+Comp+Sg+Nom/' | $LOOKUP $GTHOME/gt/sme/bin/isme.fst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
 cat attradjs compladjs superladjs >> adjs
 sort -u -o adjs adjs 
