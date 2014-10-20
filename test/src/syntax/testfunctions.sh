2d6ed97cabfbdb2c187c985ca8ff0e40cea3d8fa
Lene Antonsen <lene.antonsen@uit.no>
2014-10-20 11:11:16 +0000

    Forbedra utgave. Denne fjerner Err/Sub og unifiserer deretter, pga av problemet med ekstra analyser ved Err/Sub.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@101365 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/src/syntax/testfunctions.sh
+++ test/src/syntax/testfunctions.sh
@@ -1,17 +1,10 @@
 # A testscript: does the pipeline from fst via disambiguation to functions.cg3 function as it should?
 
 # Analyse the testcorpus:
-cat $GTHOME/langs/sme/test/data/inputCGtest.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abbr.txt | $LOOKUP $GTHOME/langs/sme/src/analyser-disamb-gt-desc.xfst | lookup2cg | vislcg3 -g $GTHOME/langs/sme/src/syntax/disambiguation.cg3 | vislcg3 -g $GTHOME/gtcore/gtdshared/smi/src/syntax/functions.cg3 > $GTHOME/langs/sme/test/data/functionstestoutput
-
-
-# Remove semantic tags, # ..
-cat $GTHOME/langs/sme/test/data/functionstestoutput | perl -pe 's/(Allegro|<vdic>|v1|v2|v3|v4|v5|v6|v7|v8|v9) //g' | tr -d "#" > $GTHOME/langs/sme/test/data/cleanfunctionstestoutput
-
-# Sort the cohorts
-perl $GTHOME/gt/script/sort-cg-cohort.pl $GTHOME/langs/sme/test/data/cleanfunctionstestoutput | uniq > $GTHOME/langs/sme/test/data/sortedfunctionstestoutput
+cat $GTHOME/langs/sme/test/data/inputCGtest.txt | preprocess --abbr=$GTHOME/gt/sme/bin/abbr.txt | $LOOKUP $GTHOME/langs/sme/src/analyser-disamb-gt-desc.xfst | lookup2cg | vislcg3 -g $GTHOME/langs/sme/src/syntax/disambiguation.cg3 | vislcg3 -g $GTHOME/gtcore/gtdshared/smi/src/syntax/korp.cg3 | perl -pe 's/(Allegro|<vdic>|v1|v2|v3|v4|v5|v6|v7|v8|v9|Err\/Sub) //g' | tr -d "#" | perl $GTHOME/gt/script/sort-cg-cohort.pl | uniq > $GTHOME/langs/sme/test/data/functionstestoutput
 
 # There should be no difference
-diff -w $GTHOME/langs/sme/test/data/functionsCG.corr.txt $GTHOME/langs/sme/test/data/sortedfunctionstestoutput > $GTHOME/langs/sme/test/data/functionstestresult
+diff -w $GTHOME/langs/sme/test/data/functionsCG.corr.txt $GTHOME/langs/sme/test/data/functionstestoutput > $GTHOME/langs/sme/test/data/functionstestresult
 see $GTHOME/langs/sme/test/data/functionstestresult
 
 
