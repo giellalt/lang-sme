47f84cf20e0cbc54305878c3aa3926d4fa7285ca
Sjur N Moshagen <sjurnm@mac.com>
2014-11-28 16:20:59 +0000

    The changes to sme lemma generation testing done during the new infra workshop on Wednesday.

--- test/src/morphology/testverblemmas.sh
+++ test/src/morphology/testverblemmas.sh
@@ -1,8 +1,8 @@
 # Kommando når man er i sme: sh test/src/morphology/testverblemmas.sh
 # Dette skriptet tester at nesten alle lemmaene i verbs.lexc kan genereres. De som ikke kan genereres, kopieres til missingverbLemmas.txt. 
 
-grep ";" $GTHOME/langs/sme/src/morphology/stems/verbs.lexc | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|LexSub)' | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > verbs
-cat verbs | sed 's/$/+V+Inf/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "V+" | grep -v "^$" | sort -u > analverbs 
-comm -23 verbs analverbs > $GTHOME/langs/sme/test/data/missingverbLemmas.txt
+grep ";" ../../../src/morphology/stems/verbs.lexc | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|LexSub)' | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > verbs
+cat verbs | sed 's/$/+V+Inf/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v "V+" | grep -v "^$" | sort -u > analverbs 
+comm -23 verbs analverbs > ../../../test/data/missingverbLemmas.txt
 rm *verbs
-see $GTHOME/langs/sme/test/data/missingverbLemmas.txt
+see ../../../test/data/missingverbLemmas.txt
