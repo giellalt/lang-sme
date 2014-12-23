50f7466acdd987f21b4b10137fef6a11ef022e02
Lene Antonsen <lene.antonsen@uit.no>
2014-12-23 10:15:18 +0000

    Tilbake til GTHOME-stier
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@104802 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/src/morphology/testadjlemmas.sh
+++ test/src/morphology/testadjlemmas.sh
@@ -2,19 +2,19 @@
 # Dette skriptet tester at nesten alle lemmaene i adjectives.lexc kan genereres. De som ikke kan genereres, kopieres til missingadjLemmas.txt
 
 #grep ";" $GTHOME/gt/sme/src/adj-sme-lex.txt | grep -v "^\!" | egrep -v "(LEXICON|ATTR|FINJU|Der|Attr| At |BBO |MUS |BUOREMUSS|OVDDIT |STUORIBUS|BUStem | K | Rreal | R )" | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > adjs
-grep ";" ../../../src/morphology/stems/adjectives.lexc | grep -v "^\!" | egrep -v "(LEXICON|ATTR|FINJU|Der|Attr| At |BBO |MUS |BUOREMUSS|OVDDIT |STUORIBUS|BUStem | K | Rreal | R |LexSub)" | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > adjs
-cat adjs | sed 's/$/+A+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u > analadjs 
-grep ";" ../../../src/morphology/stems/adjectives.lexc | grep -v "^\!" | grep -v "+Gen" | grep -v LexSub | grep "_PL " | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > pluradjs
-cat pluradjs | sed 's/$/+A+Pl+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
-grep ";" ../../../src/morphology/stems/adjectives.lexc | grep -v "^\!" | grep -v "+Gen" | grep -v LexSub | egrep "(ATTR|Attr)" | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > attradjs
-cat attradjs | sed 's/$/+A+Attr/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
-grep ";" ../../../src/morphology/stems/adjectives.lexc | grep -v "^\!" | grep "BUOREMUSS" | grep -v LexSub | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > superladjs
-cat superladjs | sed 's/$/+A+Superl+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
-grep ";" ../../../src/morphology/stems/adjectives.lexc | grep -v "^\!" | egrep "(OVDDIT|BBO |BU/MUS|EABBO/EAMOS|STUORIBUS|BUStem)" | grep -v LexSub | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > compladjs
-cat compladjs | sed 's/$/+A+Comp+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
+grep ";" $GTHOME/langs/sme/src/morphology/stems/adjectives.lexc | grep -v "^\!" | egrep -v "(LEXICON|ATTR|FINJU|Der|Attr| At |BBO |MUS |BUOREMUSS|OVDDIT |STUORIBUS|BUStem | K | Rreal | R |LexSub)" | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > adjs
+cat adjs | sed 's/$/+A+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u > analadjs 
+grep ";" $GTHOME/langs/sme/src/morphology/stems/adjectives.lexc | grep -v "^\!" | grep -v "+Gen" | grep -v LexSub | grep "_PL " | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > pluradjs
+cat pluradjs | sed 's/$/+A+Pl+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
+grep ";" $GTHOME/langs/sme/src/morphology/stems/adjectives.lexc | grep -v "^\!" | grep -v "+Gen" | grep -v LexSub | egrep "(ATTR|Attr)" | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > attradjs
+cat attradjs | sed 's/$/+A+Attr/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
+grep ";" $GTHOME/langs/sme/src/morphology/stems/adjectives.lexc | grep -v "^\!" | grep "BUOREMUSS" | grep -v LexSub | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > superladjs
+cat superladjs | sed 's/$/+A+Superl+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
+grep ";" $GTHOME/langs/sme/src/morphology/stems/adjectives.lexc | grep -v "^\!" | egrep "(OVDDIT|BBO |BU/MUS|EABBO/EAMOS|STUORIBUS|BUStem)" | grep -v LexSub | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > compladjs
+cat compladjs | sed 's/$/+A+Comp+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "A+" | grep -v "^$" | sort -u >> analadjs 
 cat attradjs compladjs superladjs >> adjs
 sort -u -o adjs adjs 
 sort -u -o analadjs analadjs 
-comm -23 adjs analadjs > ../../../test/data/missingadjLemmas.txt
+comm -23 adjs analadjs > $GTHOME/langs/sme/test/data/missingadjLemmas.txt
 rm *adjs
-see ../../../test/data/missingadjLemmas.txt
+see $GTHOME/langs/sme/test/data/missingadjLemmas.txt
--- test/src/morphology/testnounlemmas.sh
+++ test/src/morphology/testnounlemmas.sh
@@ -1,16 +1,16 @@
 # Kommando når man er i sme: sh test/src/morphology/testnounlemmas.sh
 # Dette skriptet tester at nesten alle lemmaene i nouns.lexc kan genereres. De som ikke kan genereres, kopieres til missingnounLemmas.txt
 
-grep ";" ../../../src/morphology/stems/nouns.lexc | grep -v "^\!" | egrep -v '(CmpN/Only|ShCmp|RCmpnd| Rreal | R | Rnoun |\+V\+|LexSub)' | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > nouns
-grep ";" ../../../src/morphology/stems/nouns.lexc | grep -v "^\!" | egrep -v '(ShCmp|LexSub)' |  egrep "( R | Rreal | Rnoun)" | tr ":+" " " | cut -d " " -f1 | sed 's/$/viessu/' >> nouns
-cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analnouns 
-cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Pl+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v "^$" >> analnouns 
-grep ";" ../../../src/morphology/stems/nouns.lexc | grep -v "^\!" | egrep -v '(ShCmp|LexSub)' | egrep "( R | Rreal | Rnoun)" | tr ":+" " " | cut -d " " -f1 | sed 's/$/+N+Cmp#viessu+N+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep -v '^$' >> analnouns
-grep ";" ../../../src/morphology/stems/nouns.lexc | grep -v "^\!" | grep -v ShCmp | egrep "( R | Rreal | Rnoun)" | tr ":+" " " | cut -d " " -f1 | sed 's/$/+N+Cmp#viessu+N+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep "\-" | grep "\-.*\-" | cut -d "-" -f1,2 >> analnouns
-grep ";" ../../../src/morphology/stems/nouns.lexc | grep -v "^\!" | grep -v ShCmp | grep "RHyph" | tr ":+" " " | cut -d " " -f1 | sed 's/$/+N+Cmp-#viessu+N+Sg+Nom/' | $LOOKUP ../../../src/generator-gt-norm.xfst | cut -f2 | grep "\-" | cut -d "-" -f1 >> analnouns
+grep ";" $GTHOME/langs/sme/src/morphology/stems/nouns.lexc | grep -v "^\!" | egrep -v '(CmpN/Only|ShCmp|RCmpnd| Rreal | R | Rnoun |\+V\+|LexSub)' | sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > nouns
+grep ";" $GTHOME/langs/sme/src/morphology/stems/nouns.lexc | grep -v "^\!" | egrep -v '(ShCmp|LexSub)' |  egrep "( R | Rreal | Rnoun)" | tr ":+" " " | cut -d " " -f1 | sed 's/$/viessu/' >> nouns
+cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analnouns 
+cat nouns | sed 's/$/+N+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Pl+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "^$" >> analnouns 
+grep ";" $GTHOME/langs/sme/src/morphology/stems/nouns.lexc | grep -v "^\!" | egrep -v '(ShCmp|LexSub)' | egrep "( R | Rreal | Rnoun)" | tr ":+" " " | cut -d " " -f1 | sed 's/$/+N+Cmp#viessu+N+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v '^$' >> analnouns
+grep ";" $GTHOME/langs/sme/src/morphology/stems/nouns.lexc | grep -v "^\!" | grep -v ShCmp | egrep "( R | Rreal | Rnoun)" | tr ":+" " " | cut -d " " -f1 | sed 's/$/+N+Cmp#viessu+N+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep "\-" | grep "\-.*\-" | cut -d "-" -f1,2 >> analnouns
+grep ";" $GTHOME/langs/sme/src/morphology/stems/nouns.lexc | grep -v "^\!" | grep -v ShCmp | grep "RHyph" | tr ":+" " " | cut -d " " -f1 | sed 's/$/+N+Cmp-#viessu+N+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep "\-" | cut -d "-" -f1 >> analnouns
 sort -u -o nouns nouns 
 sort -u -o analnouns analnouns 
-comm -23 nouns analnouns > ../../../test/data/missingnounLemmas.txt
+comm -23 nouns analnouns > $GTHOME/langs/sme/test/data/missingnounLemmas.txt
 rm *nouns 
-see ../../../test/data/missingnounLemmas.txt
+see $GTHOME/langs/sme/test/data/missingnounLemmas.txt
 
