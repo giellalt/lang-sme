f9e1dd990d2f4ba70457e6fd2463314832863b01
leneantonsen <lene.antonsen@uit.no>
2015-01-19 23:23:36 +0000

    Skript for å teste den disambiguerte analysen av sme-goldcorpus.txt. Fila inneholder nesten 35000 ord, og vil dermed dekke store deler av morfologien, men ikke alt. Forskjellen mellom denne testen og yaml-testene, er at den tester morfologien i løpende tekst, noe som kan være bra i mange tilfeller, f.eks. for å finne ut om former man fjerner, likevel eksisterer, eller genereringa for flere lemmaer enn det som yaml-testene dekker. Testen kjøres i to deler, før og etter endringer av analyser-disamb-gt-desc.xfst. Den må kjøres 'make' før del to kjøres. Diffen åpnes i see.

---
+++ test/src/morphology/morphology_beforetest.sh
@@ -0,0 +1,8 @@
+# Skript for å teste den disambiguerte analysen av sme-goldcorpus.txt. Testen kjøres i to deler, før og etter endringer av analyser-disamb-gt-desc.xfst. Den må kjøres 'make' før del to kjøres.
+# sh test/src/morphology/morphology_beforetest.sh 
+# sh test/src/morphology/morphology_aftertest.sh 
+
+# Analyserer testkorpus:
+cat $GTBIG/sme-goldcorpus.txt | preprocess --abbr=$GTHOME/langs/sme/tools/preprocess/abbr.txt | $LOOKUP $GTHOME/langs/sme/src/analyser-disamb-gt-desc.xfst | lookup2cg | vislcg3 -g $GTHOME/langs/sme/src/syntax/disambiguation.cg3 | perl -pe 's/ (Sem\/[^\s]+|Use\/[^\s]+|Allegro|Err\/Sub|v\d)//g' | perl -pe 's/ <[^>]+>//g' | tr -d "#" | perl $GTHOME/gt/script/sort-cg-cohort.pl | uniq > $GTHOME/langs/sme/test/data/morphologytestbefore
+
+
