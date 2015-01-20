5949f20b3f951708cb652af3afda525de6b352f2
leneantonsen <lene.antonsen@uit.no>
2015-01-20 23:38:11 +0000

    Fiksa bug i skriptet, pluss forbedringer.

--- test/src/morphology/testpropernounlemmas.sh
+++ test/src/morphology/testpropernounlemmas.sh
@@ -5,8 +5,8 @@ grep ";" $GTHOME/langs/sme/src/morphology/generated_files/smi-sme-propernouns.le
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "N+" | grep -v "^$" | sort -u > analprops 
 cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "^$" >> analprops 
 sort -u -o analprops analprops 
-cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep "N+" > $GTHOME/langs/sme/test/data/missingProperLemmas.txt
-comm -23 props analprops >> $GTHOME/gt/sme/dev/missingProperLemmas.txt
+cat props | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep "N+" | cut -d "+" -f1 | sed 's/$/+N+Prop+Pl+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep "N+" |sed 's/+Pl/+Sg/' |$LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst > $GTHOME/langs/sme/test/data/missingProperLemmas.txt
+comm -23 props analprops | sed 's/$/+N+Prop+Sg+Nom/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst >> $GTHOME/langs/sme/test/data/missingProperLemmas.txt
 grep ";" $GTHOME/langs/sme/src/morphology/generated_files/smi-sme-propernouns.lexc | grep -v "^\!" | egrep '(Attr|-atr|FirstTag)' | grep -v 'LexSub' | sed 's/% /€/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " |  sort -u > attrprops
 cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep "N+" >> $GTHOME/langs/sme/test/data/missingProperLemmas.txt
 cat attrprops | sed 's/$/+N+Prop+Attr/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 |  grep -v "N+" | grep -v "^$" | sort -u > analattrprops  
