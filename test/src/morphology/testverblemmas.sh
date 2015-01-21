a66d62d0ccc559c80831bcdb2e51f1cf75b8d768
Lene Antonsen <lene.antonsen@uit.no>
2015-01-21 09:17:02 +0000

    Forbedringer av testskript. Testen viser standardgenereringa av ord som ikke lar seg generere likt lemmaet, slik at det er fortere å debugge.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@105769 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/src/morphology/testverblemmas.sh
+++ test/src/morphology/testverblemmas.sh
@@ -3,6 +3,6 @@
 
 grep ";" $GTHOME/langs/sme/src/morphology/stems/verbs.lexc | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|LexSub|\+Neg)' | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > verbs
 cat verbs | sed 's/$/+V+Inf/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "V+" | grep -v "^$" | sort -u > analverbs 
-comm -23 verbs analverbs > $GTHOME/langs/sme/test/data/missingverbLemmas.txt
+comm -23 verbs analverbs | grep -v '^$' | sed 's/$/+V+Inf/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst > $GTHOME/langs/sme/test/data/missingverbLemmas.txt
 rm *verbs
 see $GTHOME/langs/sme/test/data/missingverbLemmas.txt
