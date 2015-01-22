771ee7866a22c66f52848fa37d27ab3ff2e20f24
Lene Antonsen <lene.antonsen@uit.no>
2015-01-22 10:19:14 +0000

    Dokumentasjon
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@105951 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/src/morphology/testverblemmas.sh
+++ test/src/morphology/testverblemmas.sh
@@ -1,8 +1,16 @@
 # Kommando når man er i sme: sh test/src/morphology/testverblemmas.sh
 # Dette skriptet tester at nesten alle lemmaene i verbs.lexc kan genereres. De som ikke kan genereres, kopieres til missingverbLemmas.txt. 
 
+# Hent ut lemmaer, bortsett fra dem med hardkoding for spesielle former, eller negasjonsverbet (som ikke har infinitivform), lagres som verbs
 grep ";" $GTHOME/langs/sme/src/morphology/stems/verbs.lexc | grep -v "^\!" | egrep -v '(STRAYFORMS|ENDLEX|LexSub|\+Neg)' | tr ":+" " " | cut -d " " -f1 | tr -d "%" | sort -u > verbs
+
+# Generer infitivsformen av verblemmaene, fjern dem som ikke gir analyse (de har V+ i output), lagres som analverbs
+
 cat verbs | sed 's/$/+V+Inf/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "V+" | grep -v "^$" | sort -u > analverbs 
+
+# Sammenlikne lista med verblemmaer med den genererte lista med infinitivsformer. Formene som er i verblemmalista, men ikke i den genererte lista, kopieres til missingverbLemmas.txt. 
 comm -23 verbs analverbs | grep -v '^$' | sed 's/$/+V+Inf/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst > $GTHOME/langs/sme/test/data/missingverbLemmas.txt
 rm *verbs
+
+# 
 see $GTHOME/langs/sme/test/data/missingverbLemmas.txt
