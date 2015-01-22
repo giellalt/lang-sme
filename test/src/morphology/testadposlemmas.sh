7c7f1bfd501d1205f86ca9d143295b0ec66d1a0b
leneantonsen <lene.antonsen@uit.no>
2015-01-22 12:43:17 +0000

    Dokumentasjon

--- test/src/morphology/testadposlemmas.sh
+++ test/src/morphology/testadposlemmas.sh
@@ -1,10 +1,24 @@
 # Kommando når man er i sme: sh test/src/morphology/testadposlemmas.sh
 # Dette skriptet tester at nesten alle lemmaene i adpositions.lexc kan genereres. De som ikke kan genereres, kopieres til missingadposLemmas.txt. 
 
-grep ";" $GTHOME/langs/sme/src/morphology/stems/adpositions.lexc | egrep -v "^(\!|\+)" | egrep -v '(LexSub)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > adpos
+
+# Hent ut lemmaer, bortsett fra LexSub (som blir filtrert bort fra normgenerator) . Lemmaene lagres som adpos
+grep ";" $GTHOME/langs/sme/src/morphology/stems/adpositions.lexc | egrep -v "^(\!|\+)" | egrep -v 'LexSub' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > adpos
+
+# Generer lemmaer med +Po
 cat adpos | sed 's/$/+Po/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "+Po" | grep -v "^$" > anadpos 
+
+# Generer med +Pr for lemmaer som ikke lar seg genereres +Po
 cat adpos | sed 's/$/+Po/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep '+Po' |sed 's/+Po/+Pr/' | $LOOKUP $GTHOME/langs/sme/src/generator-gt-norm.xfst | cut -f2 | grep -v "+Pr" | grep -v "^$" >> anadpos 
+
+# Sorter og unifiser
 sort -u -o anadpos anadpos
-comm -23 adpos anadpos > $GTHOME/langs/sme/test/data/missingadposLemmas.txt
+
+# Sammenlikne lista med adposlemmaer med den genererte lista
+comm -23 adpos anadpos > $GTHOME/langs/sme/test/data/missingadposLemmas.txt# 
+
+# Fjerne filer
 rm *adpos
+
+# Åpne fila i see
 see $GTHOME/langs/sme/test/data/missingadposLemmas.txt
