baed15d6df981f70fc7df1942e4f40b4284648d5
leneantonsen <lene.antonsen@uit.no>
2015-01-28 11:26:02 +0000

    Oppdaterte, og endra grep til egrep

--- test/src/morphology/testadvlemmas.sh
+++ test/src/morphology/testadvlemmas.sh
@@ -2,13 +2,13 @@
 # Dette skriptet tester at nesten alle lemmaene i adverbs.lexc kan genereres. De som ikke kan genereres, kopieres til missingadverbLemmas.txt. 
 
 # Hent ut lemmaer, bortsett fra LexSub (som blir filtrert bort fra normgenerator), og lemmaer som som krever Px eller kompareringstagg) . Lemmaene lagres som adverbs
-grep ";" src/morphology/stems/adverbs.lexc | grep -v "^(\!|\@|<)" | egrep -v '(LexSub|-comp| LE|-sup |SUP| Px|^adv |^adv-plc)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > adverbs
+grep ";" src/morphology/stems/adverbs.lexc | egrep -v "^(\!|\@|<|\()" | egrep -v '(LexSub|-comp| LE|-sup |SUP| Px|^adv |^adv-plc)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > adverbs
 
 # Hent ut lemmaer som krever Comp-tag. Lagres som compadverbs (se bz 1926)
-grep ";" src/morphology/stems/adverbs.lexc | grep -v "^(\!|\@|<)" | egrep -v '(LexSub| Px|^adv |^adv-plc)' | egrep '(-comp| LE)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" > compadverbs
+grep ";" src/morphology/stems/adverbs.lexc | egrep -v "^(\!|\@|<|\()" | egrep -v '(LexSub| Px|^adv |^adv-plc)' | egrep '(-comp| LE)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" > compadverbs
 
 # Hent ut lemmaer som krever Superl-tag. Lagres som supadverbs (se bz 1926)
-grep ";" src/morphology/stems/adverbs.lexc | grep -v "^(\!|\@|<)" | egrep -v '(LexSub| Px|^adv |^adv-plc)' | egrep '(-sup| SUP)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" > supadverbs
+grep ";" src/morphology/stems/adverbs.lexc | egrep -v "^(\!|\@|<|\()" | egrep -v '(LexSub| Px|^adv |^adv-plc)' | egrep '(-sup| SUP)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" > supadverbs
 
 # Generer adverbformen av lemmaene i adverbs
 sed 's/$/+Adv/' adverbs | $LOOKUP src/generator-gt-norm.xfst | cut -f2 | grep -v "Adv+" | grep -v "^$" > anadverbs 
