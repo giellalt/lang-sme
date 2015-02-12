238b5238a1a4cbea6c6be4bd6a9f30c16b9beb67
leneantonsen <lene.antonsen@uit.no>
2015-02-12 15:09:00 +0000

    Kraftig forenkling av skriptet etter at kompareringstagger er gjort valgfrie

--- test/src/morphology/testadvlemmas.sh
+++ test/src/morphology/testadvlemmas.sh
@@ -1,32 +1,14 @@
 # Kommando når man er i sme: sh test/src/morphology/testadvlemmas.sh
 # Dette skriptet tester at nesten alle lemmaene i adverbs.lexc kan genereres. De som ikke kan genereres, kopieres til missingadverbLemmas.txt. 
 
-# Hent ut lemmaer, bortsett fra LexSub (som blir filtrert bort fra normgenerator), og lemmaer som som krever Px eller kompareringstagg) . Lemmaene lagres som adverbs
-grep ";" src/morphology/stems/adverbs.lexc | egrep -v "^(\!|\@|<|\()" | egrep -v '(LexSub|-comp| LE|-sup |SUP| Px|^adv |^adv-plc)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > adverbs
-
-# Hent ut lemmaer som krever Comp-tag. Lagres som compadverbs (se bz 1926)
-grep ";" src/morphology/stems/adverbs.lexc | egrep -v "^(\!|\@|<|\()" | egrep -v '(LexSub| Px|^adv |^adv-plc)' | egrep '(-comp| LE)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" > compadverbs
-
-# Hent ut lemmaer som krever Superl-tag. Lagres som supadverbs (se bz 1926)
-grep ";" src/morphology/stems/adverbs.lexc | egrep -v "^(\!|\@|<|\()" | egrep -v '(LexSub| Px|^adv |^adv-plc)' | egrep '(-sup| SUP)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" > supadverbs
+# Hent ut lemmaer, bortsett fra LexSub (som blir filtrert bort fra normgenerator), og lemmaer som som krever Px. Lemmaene lagres som adverbs
+grep ";" src/morphology/stems/adverbs.lexc | egrep -v "^(\!|\@|<|\()" | egrep -v '(LexSub| Px|^adv |^adv-)' |sed 's/% /€/g' | sed 's/%:/¢/g' |  tr ":+" " " | cut -d " " -f1 | tr -d "%" | tr "€" " " | tr "¢" ":" | sort -u > adverbs
 
 # Generer adverbformen av lemmaene i adverbs
-sed 's/$/+Adv/' adverbs | $LOOKUP src/generator-gt-norm.xfst | cut -f2 | grep -v "Adv+" | grep -v "^$" > anadverbs 
-
-# Generer adverbformen av lemmaene i compadverbs med +Adv+Comp (se bz 1926)
-sed 's/$/+Adv+Comp/' compadverbs | $LOOKUP src/generator-gt-norm.xfst | cut -f2 | grep -v "Adv+" | grep -v "^$" >> anadverbs 
-
-# Generer adverbformen av lemmaene i supadverbs med +Adv+Superl (se bz 1926)
-sed 's/$/+Adv+Superl/' supadverbs | $LOOKUP src/generator-gt-norm.xfst | cut -f2 | grep -v "Adv+" | grep -v "^$" >> anadverbs 
-
-# Sorter og unifiser
-sort -u -o anadverbs anadverbs
-
-# Alle adverblemmaer samles i ei fil
-cat adverbs compadverbs supadverbs | sort -u > alladverbs
+sed 's/$/+Adv/' adverbs | $LOOKUP src/generator-gt-norm.xfst | grep -v "\?" | cut -f2  | grep -v "^$" | sort -u > anadverbs 
 
 # Sammenlikne lista med adverblemmaer med den genererte lista med adverber. Adverblemmaer som ikke er i den genererte lista, kopieres til missingadverbLemmas.txt. Formene generes med  +Adv for enklere debugging. 
-comm -23 alladverbs anadverbs | grep -v '^$' | sed 's/$/+Adv/' | $LOOKUP src/generator-gt-norm.xfst > test/data/missingadverbLemmas.txt
+comm -23 adverbs anadverbs | grep -v '^$' | sed 's/$/+Adv/' | $LOOKUP src/generator-gt-norm.xfst > test/data/missingadverbLemmas.txt
 
 # Fjerne lister
 rm *adverbs
