b9150b08d2ea0e9e59bfabb2ae3698a13c736652
Lene Antonsen <lene.antonsen@uit.no>
2015-02-19 12:30:32 +0000

    Endret på testene: Vi kjører dem uten cg-analyse. Poenget er å se hvilke forandringer ombygginger i lexc eller twolc gjør. Testen analyserer autentisk tekst, og dermed tester en del ting som yaml-testene ikke tester. F.eks. så har jeg funnet feil i dynamiske sammensetninger ved å bruke denne testen.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@107753 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/src/morphology/morphology_aftertest.sh
+++ test/src/morphology/morphology_aftertest.sh
@@ -1,13 +1,12 @@
-# Skript for å teste den disambiguerte analysen av sme-goldcorpus.txt. Testen kjøres i to deler, før og etter endringer av analyser-disamb-gt-desc.xfst. Det må kjøres 'make' før morphology_aftertest.sh kjøres.
+# Skript for å teste den morfologiske analysen av sme-goldcorpus.txt. Testen kjøres i to deler, før og etter endringer av analyser-gt-desc.xfst. Output kjøres via lookup2cg som rensker ut en del. Det må kjøres 'make' før morphology_aftertest.sh kjøres.
 # sh test/src/morphology/morphology_beforetest.sh 
 # sh test/src/morphology/morphology_aftertest.sh 
 
 # Analyserer testkorpus:
-cat $GTBIG/sme-goldcorpus.txt | preprocess --abbr=tools/preprocess/abbr.txt | $LOOKUP src/analyser-disamb-gt-desc.xfst | lookup2cg | vislcg3 -g src/syntax/disambiguation.cg3 | perl -pe 's/ (Sem\/[^\s]+|Use\/[^\s]+|Allegro|Err\/Sub|v\d)//g' | perl -pe 's/ <[^>]+>//g' | tr -d "#" | perl ../../gt/script/sort-cg-cohort.pl | uniq > test/data/morphologytestafter
+cat $GTBIG/sme-goldcorpus.txt | preprocess --abbr=tools/preprocess/abbr.txt | $LOOKUP src/analyser-gt-desc.xfst | lookup2cg | perl ../../gt/script/sort-cg-cohort.pl | uniq > test/data/morphologytestafter
 
 # Diff
-echo 'diff er' > test/data/morphologytest.txt
-diff -w test/data/morphologytestbefore test/data/morphologytestafter >> test/data/morphologytestdiff.txt
-see test/data/morphologytestdiff.txt
+diff -w test/data/morphologytestbefore test/data/morphologytestafter > test/data/morphologytest.txt
+see test/data/morphologytest.txt
 
 
