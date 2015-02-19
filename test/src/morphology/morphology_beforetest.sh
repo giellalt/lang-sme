b4fba74bb8c3cf07277baffd66b48f82baa18b30
leneantonsen <lene.antonsen@uit.no>
2015-02-19 12:30:32 +0000

    Endret på testene: Vi kjører dem uten cg-analyse. Poenget er å se hvilke forandringer ombygginger i lexc eller twolc gjør. Testen analyserer autentisk tekst, og dermed tester en del ting som yaml-testene ikke tester. F.eks. så har jeg funnet feil i dynamiske sammensetninger ved å bruke denne testen.

--- test/src/morphology/morphology_beforetest.sh
+++ test/src/morphology/morphology_beforetest.sh
@@ -1,8 +1,8 @@
-# Skript for å teste den disambiguerte analysen av sme-goldcorpus.txt. Testen kjøres i to deler, før og etter endringer av analyser-disamb-gt-desc.xfst. Det må kjøres 'make' før morphology_aftertest.sh kjøres.
+# Skript for å teste den morfologiske analysen av sme-goldcorpus.txt. Testen kjøres i to deler, før og etter endringer av analyser-gt-desc.xfst. Det må kjøres 'make' før morphology_aftertest.sh kjøres.
 # sh test/src/morphology/morphology_beforetest.sh 
 # sh test/src/morphology/morphology_aftertest.sh 
 
 # Analyserer testkorpus:
-cat $GTBIG/sme-goldcorpus.txt | preprocess --abbr=tools/preprocess/abbr.txt | $LOOKUP src/analyser-disamb-gt-desc.xfst | lookup2cg | vislcg3 -g src/syntax/disambiguation.cg3 | perl -pe 's/ (Sem\/[^\s]+|Use\/[^\s]+|Allegro|Err\/Sub|v\d)//g' | perl -pe 's/ <[^>]+>//g' | tr -d "#" | perl ../../gt/script/sort-cg-cohort.pl | uniq > test/data/morphologytestbefore
+cat $GTBIG/sme-goldcorpus.txt | preprocess --abbr=tools/preprocess/abbr.txt | $LOOKUP src/analyser-gt-desc.xfst | lookup2cg | perl ../../gt/script/sort-cg-cohort.pl | uniq > test/data/morphologytestbefore
 
 
