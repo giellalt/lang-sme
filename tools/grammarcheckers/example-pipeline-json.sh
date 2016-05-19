0f0de1b01c585739fb6c4d6c844c43699133d3b8
Kevin Unhammer <unhammer@fsfe.org>
2016-05-19 12:47:58 +0000

    full pipeline examples
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@134877 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

---
+++ tools/grammarcheckers/example-pipeline-json.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+hfst-tokenise --gtd "$GTHOME"/langs/sme/tools/preprocess/tokeniser-gramcheck-gt-desc.pmhfst \
+    | vislcg3 -g "$GTHOME"/langs/sme/tools/preprocess/mwe-dis.cg3 \
+    | cg-mwesplit \
+    | vislcg3 -g "$GTHOME"/langs/sme/src/syntax/valency.cg3 \
+    | vislcg3 -g "$GTHOME"/langs/sme/tools/grammarcheckers/disambiguator.cg3 \
+    | vislcg3 -g "$GTHOME"/langs/sme/tools/grammarcheckers/grammarchecker.cg3 -t \
+    | divvun-suggest \
+          --json \
+          -g "$GTHOME"/langs/sme/src/generator-gt-norm.hfstol \
+          -m "$GTHOME"/langs/sme/tools/grammarcheckers/errors.xml
