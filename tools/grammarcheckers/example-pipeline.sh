3bbdcfe83a6e3e1a0d0192c434fcfa988b8545d7
Linda Wiechetek <linda.wiechetek@uit.no>
2016-05-23 11:18:23 +0000

    added trace to disambiguation so that we can debug
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@135098 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- tools/grammarcheckers/example-pipeline.sh
+++ tools/grammarcheckers/example-pipeline.sh
@@ -4,7 +4,7 @@ hfst-tokenise --gtd "$GTHOME"/langs/sme/tools/preprocess/tokeniser-gramcheck-gt-
     | vislcg3 -g "$GTHOME"/langs/sme/tools/preprocess/mwe-dis.cg3 \
     | cg-mwesplit \
     | vislcg3 -g "$GTHOME"/langs/sme/src/syntax/valency.cg3 \
-    | vislcg3 -g "$GTHOME"/langs/sme/tools/grammarcheckers/disambiguator.cg3 \
+    | vislcg3 -g "$GTHOME"/langs/sme/tools/grammarcheckers/disambiguator.cg3 -t \
     | vislcg3 -g "$GTHOME"/langs/sme/tools/grammarcheckers/grammarchecker.cg3 -t \
     | divvun-suggest \
           -g "$GTHOME"/langs/sme/src/generator-gt-norm.hfstol \
