f6831da85a98faa4e273a518450d4497c0fc8d88
Lene Antonsen <lene.antonsen@uit.no>
2016-11-16 11:37:57 +0000

    liten forbedring
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@143661 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -1,7 +1,7 @@
 #shell script to see if there are tags which are not declared in root.lexc or if tags are misspelled
 
 echo 'Are there tags not declared in root.lexc or misspelled?'
-cat src/morphology/*lexc src/morphology/affixes/*lexc src/morphology/stems/*lexc |grep ':.* ;' |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u > lexctags
+cat src/morphology/clitics.lexc src/morphology/compounding.lexc  src/morphology/affixes/*lexc src/morphology/stems/*lexc |grep ':.* ;' |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u > lexctags
 
 cat src/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
