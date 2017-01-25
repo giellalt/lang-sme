7c38d3f363abd1ca1f80eb2ed09e5d89ba6851c3
Sjur Moshagen <sjurnm@mac.com>
2017-01-25 12:13:25 +0000

    Replaced gtcore with giella-core, or just removed it where not needed.
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@147259 c7155fb1-f0a7-4240-a2fc-2600b6f42f90

--- test/src/syntax/testfunctions.sh
+++ test/src/syntax/testfunctions.sh
@@ -1,7 +1,7 @@
 # A testscript: does the pipeline from fst via disambiguation to functions.cg3 function as it should?
 
 # Analyse the testcorpus:
-cat $GTHOME/langs/sme/test/data/inputCGtest.txt | preprocess --abbr=$GTHOME/langs/sme/tools/preprocess/abbr.txt | $LOOKUP $GTHOME/langs/sme/src/analyser-disamb-gt-desc.xfst | lookup2cg | vislcg3 -g $GTHOME/langs/sme/src/syntax/disambiguation.cg3 | vislcg3 -g $GTHOME/gtcore/giella-shared/smi/src/syntax/korp.cg3 | perl -pe 's/ (Allegro|v\d|Err\/Sub)//g' | tr -d "#" | perl $GTHOME/gt/script/sort-cg-cohort.pl | uniq > $GTHOME/langs/sme/test/data/functionstestoutput
+cat $GTHOME/langs/sme/test/data/inputCGtest.txt | preprocess --abbr=$GTHOME/langs/sme/tools/preprocess/abbr.txt | $LOOKUP $GTHOME/langs/sme/src/analyser-disamb-gt-desc.xfst | lookup2cg | vislcg3 -g $GTHOME/langs/sme/src/syntax/disambiguation.cg3 | vislcg3 -g $GTHOME/giella-shared/smi/src/syntax/korp.cg3 | perl -pe 's/ (Allegro|v\d|Err\/Sub)//g' | tr -d "#" | perl $GTHOME/gt/script/sort-cg-cohort.pl | uniq > $GTHOME/langs/sme/test/data/functionstestoutput
 
 # There should be no difference
 diff -w $GTHOME/langs/sme/test/data/functionsCG.corr.txt $GTHOME/langs/sme/test/data/functionstestoutput > $GTHOME/langs/sme/test/data/functionstestresult
