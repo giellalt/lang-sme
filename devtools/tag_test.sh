12d7b636ad651fe99397f489fc4d287efd134d8e
Lene Antonsen <lene.antonsen@uit.no>
2018-08-25 13:11:26 +0000

    jobbet litt med tagger
    git-svn-id: https://gtsvn.uit.no/langtech/trunk/langs/sme@169949 c7155fb1-f0a7-4240-a2fc-2600b6f42f90
    
    Former-commit-id: b308b29a796531ef6f5919f11b61544cfc785aef

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -1,7 +1,7 @@
 #shell script to see if there are tags which are not declared in root.lexc or if tags are misspelled
 
 echo 'Are there tags not declared in root.lexc or misspelled?'
-cat src/morphology/clitics.lexc src/morphology/compounding.lexc  src/morphology/affixes/*lexc src/morphology/stems/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |tr -s ' ' |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
+cat src/morphology/clitics.lexc src/morphology/compounding.lexc  src/morphology/affixes/*lexc src/morphology/stems/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |tr -s ' ' |sed 's/^ //' | cut -d ' ' -f1 |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"%' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
 
 cat src/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
