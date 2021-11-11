23272679776d039debd96367e5de8808b06ff515
merisiga <heiki-jaan.kaalep@ut.ee>
2021-11-11 14:41:03 +0200

    second try to classify errors

--- test/data/inspect/classify_errors.sh
+++ test/data/inspect/classify_errors.sh
@@ -8,6 +8,7 @@
 # algorithm:
 # (create the rows of a table: 1 row per 1 typo from the input file) 
 # 1) from typos.txt, remove everything except: _typo_ <tab> _correct_
+#    (plus turn @ into &atsign; because @ will be used in sed lines)
 # 2.0) relax the orthography by conflating accented letters with non-accented ones
 # 2) for any number of possible modification type, do 
 #     2.1) use a specialised transducer to generate correction candidates
@@ -15,17 +16,21 @@
 #     2.3) save the #_modification_type in a file
 # 3) combine all the files to arrive at 3 columns: typo correct #modification_type(s)
 
-# create the column of typos
+# extract the typo-correction pairs
 cat ../typos.txt.old_error_types \
 | sed 's/#.*$//' \
 | grep '.' \
+| sed 's/&amp;/\&/g' \
+| sed 's/@/\&atsign;/g' \
+> typos.txt.old
+
+# create the column of typos
+cat typos.txt.old \
 | cut -f 1 \
 > only_typos.txt
 
 # create the column of known corrects
-cat ../typos.txt.old_error_types \
-| sed 's/#.*$//' \
-| grep '.' \
+cat typos.txt.old \
 | cut -f 2 \
 > only_corrects.txt
 
@@ -142,10 +147,12 @@ do
   cp tmp3_relaxed tmp_errors_marked
 done
 
+
 # remove falsely marked errors
 # they are here because the accented letters were conflated with the non-accented ones,
-# sometimes resulting in typo=correct
+# sometimes resulting in typo being exactly the same as correct;
 # but the transducers still tagged some errors
+# in addition, remove the last line (which has sneaked in from previous buggy scripts...)
 
 paste tmp_errors_marked error_types_accents.txt \
 | sed 's/\t/@/g' \
@@ -155,31 +162,32 @@ paste tmp_errors_marked error_types_accents.txt \
 | sed '/#_a_error/s/^.*$//' \
 | sed '/#_has_space/s/^.*$//' \
 | tr -d '@' \
+| sed '$d' \
 > transducer_tagged_errors.txt
 
 #-------------
 # mark some more pairs with sed REs
 
 cat errors_marked1.txt \
-| sed '/#/!s/^\(.*h\)\([kptc]\)\(.*\)@\1\2\2\3@/&#_add1_get_double_h_kptc/' \
-| sed '/#/!s/^\(.*h\)\([kptc]\)\2\(.*\)@\1\2\3@/&#_del1_from_double_h_kptc/' \
-| sed '/#/!s/^\(.*\)\([i]\)\(.*\)@\1\2\2\3@/&#_add1_get_double_i/' \
-| sed '/#/!s/^\(.*\)\([i]\)\2\(.*\)@\1\2\3@/&#_del1_from_double_i/' \
-| sed '/#/!s/^\(.*\)\([aeou]\)\(.*\)@\1\2\2\3@/&#_add1_get_double_vowel/' \
-| sed '/#/!s/^\(.*\)\([aeou]\)\2\(.*\)@\1\2\3@/&#_del1_from_double_vowel/' \
+| sed '/#/!s/^\(.*h\)\([kptcKPTC]\)\(.*\)@\1\2\2\3@/&#_add1_get_double_h_kptc/' \
+| sed '/#/!s/^\(.*h\)\([kptcKPTC]\)\2\(.*\)@\1\2\3@/&#_del1_from_double_h_kptc/' \
+| sed '/#/!s/^\(.*\)\([iI]\)\(.*\)@\1\2\2\3@/&#_add1_get_double_i/' \
+| sed '/#/!s/^\(.*\)\([iI]\)\2\(.*\)@\1\2\3@/&#_del1_from_double_i/' \
+| sed '/#/!s/^\(.*\)\([aeouAEOU]\)\(.*\)@\1\2\2\3@/&#_add1_get_double_vowel/' \
+| sed '/#/!s/^\(.*\)\([aeouAEOU]\)\2\(.*\)@\1\2\3@/&#_del1_from_double_vowel/' \
 | sed '/#/!s/^\(.*\)\(.\)\(.*\)@\1\2\2\3@/&#_add1_get_double_cons/' \
 | sed '/#/!s/^\(.*\)\(.\)\2\(.*\)@\1\2\3@/&#_del1_from_double_cons/' \
-| sed '/#/!s/^\(.*\)\([aeiou]\)\(.*\)@\1\2[aeiuo]\3@/&#_add1_get_diphtong/' \
-| sed '/#/!s/^\(.*\)\([aeiou]\)\([aeiou]\)\(.*\)@\1\2\4@/&#_del1_from_diphtong/' \
+| sed '/#/!s/^\(.*\)\([aeiouAEIOU]\)\(.*\)@\1\2[aeiuoAEIOU]\3@/&#_add1_get_diphtong/' \
+| sed '/#/!s/^\(.*\)\([aeiouAEIOU]\)\([aeiouAEIOU]\)\(.*\)@\1\2\4@/&#_del1_from_diphtong/' \
 | sed '/#/!s/^\(.*\)\(.*\)@\1.\2@/&#_add1/' \
 | sed '/#/!s/^\(.*\)\(.\)\(.*\)@\1\3@/&#_del1/' \
 \
 | sed '/#/!s/^\(.*\)\([kptc]\)\2\(.*\)@\1\([gbdz]\)\4\3@/&#_subst_both_kptc/' \
 | sed '/#/!s/^\(.*\)\([gbdz]\)\2\(.*\)@\1\([kptc]\)\4\3@/&#_subst_both_gbdz/' \
 \
-| sed '/#/!s/^\(.*\)[aeiou]\(.*\)@\1[aeiou]\2@/&#_subst1_vowel/' \
-| sed '/#/!s/^\(.*\)[kpt]\(.*\)@\1[gbd]\2@/&#_subst1_kpt_get_gbd/' \
-| sed '/#/!s/^\(.*\)[gbd]\(.*\)@\1[kpt]\2@/&#_subst1_gbd_get_kpt/' \
+| sed '/#/!s/^\(.*\)[aeiouAEIOU]\(.*\)@\1[aeiouAEIOU]\2@/&#_subst1_vowel/' \
+| sed '/#/!s/^\(.*\)[kptKPT]\(.*\)@\1[gbdGBD]\2@/&#_subst1_kpt_get_gbd/' \
+| sed '/#/!s/^\(.*\)[gbdGBD]\(.*\)@\1[kptKPT]\2@/&#_subst1_gbd_get_kpt/' \
 | sed '/#/!s/^\(.*\).\(.*\)@\1.\2@/&#_subst1/' \
 | sed '/#/!s/^\([^ ]*\)\([^ ]\)\([^ ]\)\([^ ]*\)@\1\3\2\4@/&#_trans1/' \
 \
@@ -192,18 +200,6 @@ cat errors_marked1.txt \
 | sed '/#/!s/^\([^ ]*\)[^ ]\([^ ]*\)@\1[^ ][^ ]\2@/&#_subst1to2/' \
 | sed '/#/!s/^\([^ ]*\)[^ ][^ ]\([^ ]*\)@\1[^ ]\2@/&#_subst2to1/' \
 > errors_marked2.txt
-
-# make non-accent-related error column
-# to be later combined with other accent error types
-#cat errors_marked2.txt \
-#| sed 's/^[^@]*@[^@]*@//' \
-#> error_types_of_relaxed.txt
-
-# combine accent and other error types
-#paste error_types_accents.txt error_types_of_relaxed.txt \
-#| sed '/#/!s/$/#_unknown_type/' \
-#| sed 's/\t/@/g' \
-#> error_types.txt
 
 # combine error type tag from different sources
 # and remove some redundant classifications
@@ -219,24 +215,11 @@ cat errors_marked2.txt \
 | sed 's/#_del2_adjacent#_repetition/#_repetition/' \
 | sed 's/#_subst[^#]*#_transpose/#_transpose/' \
 | sed 's/#_[^#]*#_repetition/#_repetition/' \
+| sed 's/\(#_[^#]*\)#\(_subst1_kb_next\)/\1\/\2/' \
+| sed 's/\(#_no_error\).*$/\1/' \
+| sed '/#_has_space/s/^\([^#]*\)#.*$/\1#_has_space/' \
 \
-| sed '/#/!s/$/#_unknown_type/' \
 > error_types.txt
-
-# add accent error note to those who have some other error too
-paste only_typos.txt only_relaxed_typos.txt \
-| sed 's/\t/@/g' \
-| sed 's/^\([^@]*\)@\1$/&@#accents_ok/' \
-| sed '/#accents_ok/!s/$/@#accent_error/' \
-| sed 's/^[^@]*@[^@]*@//' \
-| paste error_types_accents.txt - \
-| sed 's/\t/@/g' \
-| sed 's/^\(#[^@]*\)@.*$/\1/' \
-| sed 's/^@#accents_ok//' \
-| sed 's/@\(#accent\)/#\1/' \
-| sort | uniq -c
-
-
 
 paste only_typos.txt only_corrects.txt \
 | paste - error_types.txt \
@@ -245,31 +228,21 @@ paste only_typos.txt only_corrects.txt \
 # create a 3-column table with some type for every word
 # (in case of multiple types, keep only the first one on the line)
 cat err_types.txt \
-| sed 's/^\t@#.*$//' | sed 's/#[^@]*@/@/' | sed 's/@$/@#_complex/' | sed 's/@#_/# /g' \
-| sed 's/#\([^#]*\) *#.*$/#\1/'\
+| sed '/#/!s/$/\t#_unknown_type/' \
+| sed 's/&atsign;/@/g' \
 > err_types_all_words.txt
 
 exit
 
-#some attempts to understand Sami orthography...
-#proovin liigitada astmevahelduse malle
+algne:
+# create a 3-column table with some type for every word
+# (in case of multiple types, keep only the first one on the line)
+cat err_types.txt \
+| sed 's/^\t@#.*$//' | sed 's/#[^@]*@/@/' | sed 's/@$/@#_unknown_type/' | sed 's/@#_/# /g' \
+| sed 's/#\([^#]*\) *#.*$/#\1/'\
+| sed 's/&atsign;/@/g' \
+> err_types_all_words.txt
 
-cat oahpa_clusters.txt | cut -f 1,2 | grep -v '^#' | tr -d ' ' | grep '.' | LC_COLLATE=C sort | sed 's/\t/@/' | sed 's/^\(.*\)\(.\)@\1\2\2$/&#final2double/' | sed 's/^\(.*\)@\1$/&#nochange/' | sed 's/^\(.\)\1@\(.\)\2$/&#both2other/' | sed 's/^\(.*\)\([bdg]\)\(.*\)@\1[mnŋ]\3$/&#bdg2mnn/' | sed 's/^\(.*\)\([bdg]\)\(.*\)@\1[ptk]\3$/&#bdg2ptk/' | sed 's/^\(.*\)\(.\)\2@\1\2$/&#doublefinal2one/' | sed 's/^[k]\(.*\)\(.\)@v\1\2\2$/&#k2v/' | sed 's/^\(.\)\1j@\1j$/&#firstdouble2one/' > oahpa_clusters_types.txt
-
-#teen mallidest mustrid, et vigu liigitada
-cat oahpa_clusters_types.txt \
-| sed 's/\t/@/g' | sed 's/^\(.*\)@\(.*\)#\(.*\)$/sed "s\/^\\(.*\\)\1\\(.*\\)@\\1\2\\2@\/\&#\3\/"/' \
-
-#tee maalidest sedi mustrifail
-cat oahpa_clusters_types.txt \
-| sed 's/\t/@/g' | sed 's/^\(.*\)@\(.*\)#\(.*\)$/s\/^\\(.*\\)\1\\(.*\\)@\\1\2\\2@\/\&#3rd_to_2nd\3\//' > 3rd_to_2nd.sed
-
-cat oahpa_clusters_types.txt \
-| sed 's/\t/@/g' | sed 's/^\(.*\)@\(.*\)#/\2@\1#/' | sed 's/^\(.*\)@\(.*\)#\(.*\)$/s\/^\\(.*\\)\1\\(.*\\)@\\1\2\\2@\/\&#2nd_to_3rd\3\//' > 2nd_to_3rd.sed
-
-
-# mustrifaili kasutamine:
-cat tmp_errors_marked | sed 's/\t/@/g' | sed -f 3rd_to_2nd.sed
 
 # more detailed look at a type
 
@@ -312,33 +285,6 @@ cat errors_marked2.txt | grep 'del1$' | sed 's/#.*$//' | sed 's/@/ /g' | sed 's/
       1 eu->e
       1 au->a
 
-cat errors_marked2.txt | grep 'subst1to2$' | sed 's/#.*$//' | sed 's/@/ /g' | sed 's/ $//' \
-| gawk '{max=length($1); i=0; j=0; for ( ; i <=max; ) {i++; j++; v11=substr($1, i, 1); v12=substr($1, i+1, 1); v21=substr($2, j, 1); v22=substr($2, j+1, 1); printf("%s", v11); if (v11 != v21){{j++; printf(":%s%s", v21, v22)}} } printf("\n")}' \
-| sed 's/\(.:..\)/ \1 /g' | tr ' ' '\n' | grep ':' | sort | uniq -c | sort -nr | less
 
 
-
-cat errors_marked2.txt | grep '#_subst1$' | sed 's/#.*$//' | sed 's/@/ /g' \
-| sed 's/ $//' \
-| gawk '{max=length($1); for (i=1; i <=max; i++){v1=substr($1, i, 1); v2=substr($2, i, 1); printf("%s", v1); if (v1 != v2){printf(":%s", v2)} } printf("\n")}' | sed 's/\(.:.\)/ \1 /g' | tr ' ' '\n' | grep ':' | sort | uniq -c | sort -nr | less
-
-cat errors_marked2acdst.txt | grep '#_subst2' | sed 's/#.*$//' | sed 's/@/ /g' \
-| sed 's/ $//' \
-| gawk '{max=length($1); for (i=1; i <=max; i++){v11=substr($1, i, 1); v12=substr($1, i+1, 1); v21=substr($2, i, 1); v22=substr($2, i+1, 1); printf("%s%s", v11, v12); if (v11 != v21){if (v12 != v22){printf(":%s%s", v21, v22)}} } printf("\n")}' | sed 's/\(..:..\)/ \1 /g' | tr ' ' '\n' | grep ':' | sort | uniq -c | sort -nr | less
-
-cat errors_marked2.txt | grep '#_subst1to2' | sed 's/#.*$//' | sed 's/@/ /g' \
-| sed 's/ $//' \
-| gawk '{max=length($1); i=0; j=0; for ( ; i <=max; ) {i++; j++; v11=substr($1, i, 1); v12=substr($1, i+1, 1); v21=substr($2, j, 1); v22=substr($2, j+1, 1); printf("%s", v11); if (v11 != v21){{j++; printf(":%s%s", v21, v22)}} } printf("\n")}' | sed 's/\(.:..\)/ \1 /g' | tr ' ' '\n' | grep ':' | sort | uniq -c | sort -nr | less
-
-cat errors_marked2acdst.txt | grep 'one2double' | sed 's/#.*$//' | sed 's/@/ /g' \
-| sed 's/ $//' \
-| gawk '{max=length($1); for (i=1; i <=max; i++){v1=substr($1, i, 1); v2=substr($2, i, 1); printf("%s", v1); if (v1 != v2){printf(":%s", v2)} } printf("\n")}' | sed 's/\(.:.\)/ \1 /g'
-
-# make a regex file
-cat errors_marked2.txt | grep '#_subst1$' | sed 's/#.*$//' | sed 's/@/ /g' \
-| sed 's/ $//' \
-| gawk '{max=length($1); for (i=1; i <=max; i++){v1=substr($1, i, 1); v2=substr($2, i, 1); printf("%s", v1); if (v1 != v2){printf(":%s", v2)} } printf("\n")}' | sed 's/\(.:.\)/ \1 /g' | tr ' ' '\n' | grep ':' | sort | uniq -c \
-| sed 's/:/ (->) /' | sed 's/$/::1 ,/' 
-
-#
 
---
+++ test/data/inspect/modify_typos_txt.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# !!! ---- NB! ---- !!! overwrites ../typos.txt
+# !!! ---- NB! ---- !!! be sure this is what you want!
+
+# input:  old version of typos.txt file
+#         err_types_all_words.txt
+# output: new version of typos.txt 
+#
+
+# count the tags
+cat err_types_all_words.txt \
+| grep '#' | sed 's/^[^#]*#/#/' | sed 's/#/\n#/g' | sed 's/ $//' \
+| sort | uniq -c \
+> err_types_counted.txt
+
+# create a new typos.txt file
+cp  ../typos.txt.new_error_types.header ../typos.txt
+
+# turn the counted tags file into documentation
+cat err_types_counted.txt | head -1 | sed 's/^/# corpus size:/' >> ../typos.txt
+echo '# error types:' >> ../typos.txt
+cat err_types_counted.txt | grep '#' | sed 's/^/#/' >> ../typos.txt
+
+cat err_types_all_words.txt >> ../typos.txt
+
+exit
+
+# some example filters for 
+# more detailed look at a type
+
+# make a frequency list for the letters that get substituted
+cat errors_marked2.txt | grep '#_subst1$' | sed 's/#.*$//' | sed 's/@/ /' | gawk '{max=length($1); for (i=1; i <=max; i++){v1=substr($1, i, 1); v2=substr($2, i, 1); printf("%s", v1); if (v1 != v2){printf(":%s", v2)} } printf("\n")}' | sed 's/\(.:.\)/ \1 /g' | tr ' ' '\n' | grep ':' | sort | uniq -c | sort -nr | less
+
+# frequency list of letters that have been omitted (and thus they need to be added to arrive at the correct word)
+
+cat errors_marked2.txt | grep '#_add1' | sed 's/#.*$//' | sed 's/@/ /g' | sed 's/ $//' | sed 's/^\(.*\)\(.*\) \1\(.\)\2$/\1\2 \1\n\3:\n\2/' | grep ':' | sort | uniq -c | sort -nr | less
+
+# frequency list of letters of correct words
+cat only_corrects.txt | sed 's/./\n&\n/g' | grep '.' | sort | uniq -c | sort -nr | less
+
+# --> they are rather similar; 
+# a and s top the list of omitted ones probably because they are typed with the small finger of the left hand...
+
+# notes
+strings.regex: linguistic errors:
+otsusi otsuseid
+kumba kumma
+töötleda töödelda
+gbd <-> kpt
+
+?
+ühekordne <-> topelt
+töödata töötada # või transpos dat -> tad
+
+kombinatsioonid:
+täpitäht + trükkimisega seotu 
+transpos + ühekordne <-> topelt
+transpos + transpos
+
+praegu välja jäetud:
+haruldane asendus, nt. n:l hüdraunuline
+                       v:s mõistuv mõistus
+
+
+2-3 kõrvuti tähte lisatud
+pikem korduvus
+suurema kaugusega transposits 
+---
+...miset -> miste, mist, mised, mise, misest
+----
+
+---
+
+
