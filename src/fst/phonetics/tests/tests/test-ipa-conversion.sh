997dc7c56a8597603f8f2291ee2c4305f968554a
Flammie A Pirinen <flammie@iki.fi>
2024-01-21 18:14:24 +0100

    [Template merge] src/fst reorg

---
+++ src/fst/phonetics/tests/tests/test-ipa-conversion.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###### Variables: #######
+IPAFST=../text2ipa.hfst
+
+#### Actual test: ####
+if test -f $INPUTFILE ; then
+    if ! /usr/bin/hfst-lookup -q $IPAFST < $INPUTFILE > $OUTPUTFILE ; then
+        echo turning $INPUTFILE into IPA failed!
+        exit 1
+    fi
+    if ! diff $OUTPUTFILE $EXPECTFILE > /dev/null 2>&1 ; then
+        echo $OUTPUTFILE differs from $EXPECTFILE
+        exit 1
+    fi
+    rm $OUTPUTFILE
+else
+    echo $INPUTFILE not found!
+    exit 77
+fi
