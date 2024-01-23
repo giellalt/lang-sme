3d60a6d93c2996b309ee2dad1f1ed254a59896bb
leneantonsen <lene.antonsen@uit.no>
2024-01-23 17:25:19 +0100

    oppdaterte stier

--- devtools/tag_test.sh
+++ devtools/tag_test.sh
@@ -1,20 +1,20 @@
 #shell script to see if there are tags which are not declared in root.lexc or if tags are misspelled
 
 echo 'Possible tags not declared in root.lexc or misspelled:'
-cat src/fst/clitics.lexc src/fst/compounding.lexc  src/fst/affixes/*lexc src/fst/stems/*lexc  ../shared-smi/src/fst/stems/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |rev |cut -d ' ' -f1 |rev |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
+cat src/fst/morphology/clitics.lexc src/fst/morphology/compounding.lexc  src/fst/morphology/affixes/*lexc src/fst/morphology/stems/*lexc  ../shared-smi/src/fst/morphology/stems/*lexc |cut -d '!' -f1 |grep ' ;' | cut -d ':' -f1 |rev |cut -d ' ' -f1 |rev |sed 's/+/¢+/g' |sed 's/@/¢@/g'|tr '¢' '\n' | tr '#"' '\n'| egrep '(\+|@)' |sort -u | egrep -v '^(\+|\+%|\+\/\-|\+Cmp\-|\+Cmp%\-|\@0|\@%)$' > lexctags
 
-cat src/fst/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
+cat src/fst/morphology/root.lexc |cut -d '!' -f1 |cut -d ':' -f1 |sed 's/+/¢+/g'|sed 's/@/¢@/g' |tr '¢' '\n' | egrep '(\+|@)' |tr -d ' ' | tr -d '\t'|sort -u > roottags
 
 comm -23 lexctags roottags 
 
 echo 'Checking for double semicolon in stem files:'
-cat src/fst/stems/*lexc |cut -d '!' -f1 |grep ';.*;'
+cat src/fst/morphology/stems/*lexc |cut -d '!' -f1 |grep ';.*;'
 
 echo 'Checking for missing Der-tags:'
-cat src/fst/stems/*lexc |cut -d '!' -f1 |grep '\+Der/.*;' |egrep -v 'Der([1234]|\+)'
+cat src/fst/morphology/stems/*lexc |cut -d '!' -f1 |grep '\+Der/.*;' |egrep -v 'Der([1234]|\+)'
 
 echo 'Checking for double Sem-tags:'
-cat src/fst/stems/*lexc |cut -d '!' -f1 |grep '+Sem.*+Sem' 
+cat src/fst/morphology/stems/*lexc |cut -d '!' -f1 |grep '+Sem.*+Sem' 
 
 
 echo 'checked'
