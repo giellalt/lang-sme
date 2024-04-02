0d9247c297c27eae5de571bd263a6540eacd2941
leneantonsen <lene.antonsen@uit.no>
2024-04-02 09:23:15 +0200

    for numeraler

---
+++ devtools/num_minip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in smn:
+# sh devtools/noun_minip.sh LAAVU | less
+# sh devtools/noun_minip.sh smiergâs 
+# Only get the lemma you ask for:
+# sh devtools/noun_minip.sh '^smiergâs[:+]' 
+
+
+HLOOKUP=$(echo $HLOOKUP)
+GTLANGS=$(echo $GTLANGS)
+
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/fst/morphology/stems/nouns.lexc | egrep $PATTERN | grep -v Err | tr '+' ':'| cut -d ':' -f1>$L_FILE
+
+P_FILE="test/data/testnumparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+   echo "${lemma}${form}" | $HLOOKUP $GTLANGS/lang-sme/src/fst/generator-gt-norm.hfstol
+ done
+ rm -f $L_FILE
+done
+
