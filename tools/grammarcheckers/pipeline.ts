598a265527567795ead8c2d0aa27afdcf419a7db
Børre Gaup <albbas@gmail.com>
2025-12-08 21:09:27 +0100

    We need a pipeline for the development version of grammarchecker

--- tools/grammarcheckers/pipeline.ts
+++ tools/grammarcheckers/pipeline.ts
@@ -21,6 +21,24 @@ export default function smeGramRelease(entry: StringEntry): Command {
   return divvun.suggest("suggestions", x, { model_path: "generator-gramcheck-gt-norm.hfstol" });
 }
 
+export function smeGram(entry: StringEntry): Command {
+  let x = hfst.tokenize("tokenize", entry, { model_path: "tokeniser-gramcheck-gt-desc.pmhfst" });
+  x = divvun.blanktag("whitespace", x, { model_path: "analyser-gt-whitespace.hfst" });
+  x = cg3.vislcg3("valency", x, { model_path: "valency.bin" });
+  x = cg3.vislcg3("mwe-dis", x, { model_path: "mwe-dis.bin" });
+  x = cg3.mwesplit("mwesplit", x);
+  x = divvun.blanktag("errorwhitespace", x, { model_path: "analyser-gt-errorwhitespace.hfst" });
+  x = divvun.cgspell("speller", x, {
+    acc_model_path: "acceptor.default.hfst",
+    err_model_path: "errmodel.default.hfst",
+  });
+  x = cg3.vislcg3("postspell-valency", x, { model_path: "valency-postspell.bin" });
+  x = cg3.vislcg3("grc-disamb", x, { model_path: "grc-disambiguator.bin" });
+  x = cg3.vislcg3("spell-sugg-filtering", x, { model_path: "spellchecker.bin" });
+  x = cg3.vislcg3("gramcheck", x, { model_path: "grammarchecker.bin" });
+  return divvun.suggest("suggestions", x, { model_path: "generator-gramcheck-gt-norm.hfstol" });
+}
+
 /**
  * Dev pipeline for testing with local models.
  */
