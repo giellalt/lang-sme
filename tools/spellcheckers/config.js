62accc8bd32f222cacf17041c85a0a93cb56991b
Sjur N Moshagen <sjurnm@mac.com>
2026-02-25 13:13:20 +0200

    divvunspell config file that gives good results
    Precision: 93.06%
    Recall: 92.62%
    Accuracy: 27.59%
    
    With the default values the results are:
    
    Precision: 94.36%
    Recall: 93.92%
    Accuracy: 9.59%
    
    That is, by loosing slightly in precision and recall, we gain a lot in accuracy, basically throwing out a lot of garbage suggestions, and only a few good ones. The overall experience of the speller should be much improved (the lost suggestions are mostly way down in the suggestion list, and lost on the user in any case).

---
+++ tools/spellcheckers/config.js
@@ -0,0 +1,13 @@
+{
+  "n-best": 10,
+  "max-weight": 10000,
+  "beam": 29,
+  "reweight": {
+    "start-penalty": 20,
+    "end-penalty": 10,
+    "mid-penalty": 5
+  },
+  "node-pool-size": 128,
+  "recase": true,
+  "completion-marker": null
+}
