fc211587545e4afeb7a80222434d23ec7eb9bbcd
Helenao <helena.omma@uit.no>
2026-04-01 13:02:49 +0200

    skrev in namn på riktig fil

--- devtools/num_minip.sh
+++ devtools/num_minip.sh
@@ -16,7 +16,7 @@ GTLANGS=$(echo $GTLANGS)
 
 PATTERN=$1
 L_FILE="in.txt"
-cut -d '!' -f1 src/fst/morphology/stems/nouns.lexc | egrep $PATTERN | grep -v Err | tr '+' ':'| cut -d ':' -f1>$L_FILE
+cut -d '!' -f1 src/fst/morphology/stems/numerals.lexc | egrep $PATTERN | grep -v Err | tr '+' ':'| cut -d ':' -f1>$L_FILE
 
 P_FILE="src/fst/morphology/test/testnumparadigm.txt"
 
