03e88e74637975f17ede01a1f529aba78e3891bd
Trondtr <trond.trosterud@uit.no>
2026-09-03 17:05:57 +0200

    updated, not taking xmsy into account

--- tools/grammarcheckers/check-explanation-tags.sh
+++ tools/grammarcheckers/check-explanation-tags.sh
@@ -6,89 +6,64 @@
 echo "\n"
 echo "Are the rule tags in grammarchecker.cg3 in use and declared in the error tags files?"
 echo "=========================================================================\n"
-cat grammarchecker.cg3 |grep "^LIST &"|cut -d" " -f2|tr -d "&"|sort | uniq > xxg
+cat grammarchecker.cg3 |grep "^LIST &"|tr -s " "|cut -d" " -f2|tr -d "&"|sort | uniq > xxd # defined
 cat grammarchecker.cg3 |grep "^ADD:"|tr "\t" " "| tr -s " "| cut -d" " -f1|cut -d":" -f2|sort|uniq > xxb   # in use
-cat errors-se.ftl |grep "^[a-z]"|cut -d" " -f1|sort | uniq > xxn
-cat errors-en.ftl |grep "^[a-z]"|cut -d" " -f1|sort | uniq > xxe
-cat errors.json |grep '"id"'|cut -d'"' -f4|sort | uniq > xxj
-cat errors.source.xml | grep "error id"|cut -d'"' -f2|sort | uniq > xxx
-
-echo "Number of tags not in use: "
-comm -23 xxg xxb | wc -l
-echo "The tags declared but not used in the grammarchecker file are:"
-comm -23 xxg xxb
-echo "\n"
-
-
-echo "Number of tags missing in errors-se.ftl: "
-comm -23 xxg xxn | wc -l
-echo "The tags tags missing in the native ftl file were:"
-comm -23 xxg xxn
-echo "\n"
-
-echo "Number of tags missing in errors-en.ftl: "
-comm -23 xxg xxe | wc -l
-echo "The tags tags missing in errors-en.ftl file were:"
- comm -23 xxg xxe
-echo "\n"
- 
-echo "Number of tags missing in errors.json: "
-comm -23 xxg xxj | wc -l
-echo "The tags issing in errors.json were: "
-comm -23 xxg xxj
-echo "\n"
-
-echo "Number of tags missing in errors.source.xml: "
-comm -23 xxg xxx | wc -l
-echo "The tags missing in errors.xml were: "
-comm -23 xxg xxx
-echo "\n"
-
-# summary
-
-echo "\n"
-echo "Summary"
+cat errors-se.ftl |grep "^[a-z]"|cut -d" " -f1|sort | uniq > xxn # native
+cat errors-en.ftl |grep "^[a-z]"|cut -d" " -f1|sort | uniq > xxe # emglish
+cat errors.json |grep '"id"'|cut -d'"' -f4|sort | uniq > xxj #json
+cat errors.source.xml | grep "error id"|cut -d'"' -f2|sort | uniq > xxx # xml
 
 echo " Tags defined:"
-cat xxg |wc -l
+cat xxd |wc -l
 
 echo " Tags used:"
 cat xxb |wc -l 
 
+echo "Number of tags not in use: "
+comm -13 xxb xxd | wc -l
+echo "Number of undefined tags: "
+comm -23 xxb xxd | wc -l
+
+echo "The tags declared but not used in the grammarchecker file are:"
+comm -23 xxd xxb | tr "\n" ","|sed 's/,/, /g;'
+echo "\n"
+
+
 echo " Tags in native ftl:"
 cat xxn |wc -l 
+
+echo "Number of tags missing in errors-se.ftl: "
+comm -23 xxd xxn | wc -l
+echo "The tags tags missing in the native ftl file were:"
+comm -23 xxd xxn | tr "\n" ","|sed 's/,/, /g;'
+echo "\n"
 
 echo " Tags in en ftl:"
 cat xxe |wc -l 
 
-echo "Tags in json"
-cat xx |wc -l 
-
-echo "Tags in xml"
-cat xxx |wc -l 
-
-
-
-echo "Number of tags not in use: "
-comm -23 xxg xxb | wc -l
-echo "\n"
-
-echo "Number of tags missing in errors-se.ftl: "
-comm -23 xxg xxn | wc -l
-echo "\n"
-
 echo "Number of tags missing in errors-en.ftl: "
-comm -23 xxg xxe | wc -l
+comm -23 xxd xxe | wc -l
+echo "The tags tags missing in errors-en.ftl file were:"
+ comm -23 xxd xxe | tr "\n" ","|sed 's/,/, /g;'
 echo "\n"
  
+echo "Tags in json:"
+cat xxjx |wc -l 
 echo "Number of tags missing in errors.json: "
-comm -23 xxg xxj | wc -l
+comm -23 xxd xxj | wc -l
+echo "The tags issing in errors.json were: "
+comm -23 xxd xxj | tr "\n" ","|sed 's/,/, /g;'
 echo "\n"
 
+echo "Tags in xml:"
+cat xxx |wc -l 
 echo "Number of tags missing in errors.source.xml: "
-comm -23 xxg xxx | wc -l
+comm -23 xxd xxx | wc -l
+echo "The tags missing in errors.xml were: "
+comm -23 xxd xxx | tr "\n" ","|sed 's/,/, /g;'
 echo "\n"
+
 
 #echo "Delete auxiliary xx* files\n"
-rm -f xxb xxe xxn xxg xxj xxx 
+#rm -f xxb xxe xxn xxd xxj xxx 
 
